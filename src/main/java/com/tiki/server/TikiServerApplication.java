26dff264cec47bd6be93172aa6a580fbda801cef
Chan531 <chan657312@gmail.com>
2024-06-24 23:15:57 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/com/tiki/server/TikiServerApplication.java
@@ -0,0 +1,13 @@
+package com.tiki.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiki/server/TikiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiki.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
