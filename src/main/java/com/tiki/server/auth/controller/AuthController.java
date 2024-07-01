35ba76d5584ab0599955f6fabcd5d572ef336be5
Chan531 <chan657312@gmail.com>
2024-07-02 03:52:34 +0900

    [INIT] Auth 패키지 초기 세팅

---
+++ src/main/java/com/tiki/server/auth/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.tiki.server.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiki.server.auth.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+	private final AuthService authService;
+}
