734ad36ef6df8461b57905512f5c45831505ff19
Chan531 <chan657312@gmail.com>
2024-07-02 03:50:20 +0900

    [INIT] BaseTime 초기 세팅

---
+++ src/main/java/com/tiki/server/common/entity/BaseTime.java
@@ -0,0 +1,23 @@
+package com.tiki.server.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+
+	@CreatedDate
+	protected LocalDateTime createdAt;
+
+	@LastModifiedDate
+	protected LocalDateTime updatedAt;
+}
