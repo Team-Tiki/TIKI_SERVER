578a15e25d85405449a7f4e24879b162b8a579cf
Chan531 <chan657312@gmail.com>
2024-07-10 03:03:47 +0900

    [CHORE] 임시 커밋

--- src/main/java/com/tiki/server/timeblock/controller/TimeBlockController.java
+++ src/main/java/com/tiki/server/timeblock/controller/TimeBlockController.java
@@ -1,11 +1,20 @@
 package com.tiki.server.timeblock.controller;
 
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tiki.server.timeblock.dto.response.TimeBlockCreationResponse;
 import com.tiki.server.timeblock.service.TimeBlockService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,4 +22,16 @@ import lombok.RequiredArgsConstructor;
 public class TimeBlockController {
 
 	private final TimeBlockService timeBlockService;
+
+	@PostMapping("/team/{teamId}/time-block")
+	public ResponseEntity<TimeBlockCreationResponse> createTimeBlock(
+		Principal principal,
+		@PathVariable("teamId") long teamId,
+		@RequestParam String type,
+		@RequestBody
+	) {
+		val memberId = Long.valueOf(principal.getName());
+		val response = timeBlockService.createTimeBlock()
+		return ResponseEntity.created(URI);
+	}
 }
