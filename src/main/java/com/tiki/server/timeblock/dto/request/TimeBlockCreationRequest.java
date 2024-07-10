89967488a0aadbd08f10943bd6b9c0d163cabffb
Chan531 <chan657312@gmail.com>
2024-07-11 00:10:40 +0900

    [ADD] TimeBlock 생성 dto 추가

--- src/main/java/com/tiki/server/timeblock/dto/request/TimeBlockCreationRequest.java
+++ src/main/java/com/tiki/server/timeblock/dto/request/TimeBlockCreationRequest.java
@@ -1,4 +1,15 @@
 package com.tiki.server.timeblock.dto.request;
 
-public record TimeBlockCreationRequest() {
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.NonNull;
+
+public record TimeBlockCreationRequest(
+	@NonNull String name,
+	@NonNull String color,
+	@NonNull LocalDate startDate,
+	@NonNull LocalDate endDate,
+	List<String> filesUrl
+) {
 }
--- src/main/java/com/tiki/server/timeblock/dto/response/TimeBlockCreationResponse.java
+++ src/main/java/com/tiki/server/timeblock/dto/response/TimeBlockCreationResponse.java
@@ -1,4 +1,17 @@
 package com.tiki.server.timeblock.dto.response;
 
-public record TimeBlockCreationResponse() {
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.Builder;
+
+@Builder(access = PRIVATE)
+public record TimeBlockCreationResponse(
+	long timeBlockId
+) {
+
+	public static TimeBlockCreationResponse of(long timeBlockId) {
+		return TimeBlockCreationResponse.builder()
+			.timeBlockId(timeBlockId)
+			.build();
+	}
 }
