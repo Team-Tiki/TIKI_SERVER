925828a65c4eed622250845a2f3ed19cede4efef
Chan531 <chan657312@gmail.com>
2024-07-11 00:11:21 +0900

    [ADD] TimeBlock 관련 상수 추가

---
+++ src/main/java/com/tiki/server/timeblock/constant/TimeBlockConstant.java
@@ -0,0 +1,7 @@
+package com.tiki.server.timeblock.constant;
+
+public class TimeBlockConstant {
+
+	public static final String EXECUTIVE = "executive";
+	public static final String MEMBER = "member";
+}
