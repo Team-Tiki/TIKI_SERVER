a4de3a6b1f0650fa4464c78d569085c2a4e67762
paragon0107 <smg0725@naver.com>
2024-07-11 23:27:05 +0900

    [CHORE] 아이템 명 변경

--- src/main/java/com/tiki/server/member/dto/request/MemberProfileCreateRequest.java
+++ src/main/java/com/tiki/server/member/dto/request/MemberProfileCreateRequest.java
@@ -1,13 +1,14 @@
 package com.tiki.server.member.dto.request;
 
 import com.tiki.server.common.entity.School;
+import com.tiki.server.common.entity.University;
 import lombok.Data;
 import lombok.NonNull;
 
 public record MemberProfileCreateRequest(
         @NonNull String name,
         @NonNull Data birth,
-        @NonNull School school,
+        @NonNull University Univ,
         @NonNull String email,
         @NonNull String password,
         @NonNull String passwordCk
