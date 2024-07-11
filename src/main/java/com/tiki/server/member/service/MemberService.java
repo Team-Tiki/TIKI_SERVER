c2da3046741e04d8405ae64cb0f782a822257320
paragon0107 <smg0725@naver.com>
2024-07-12 00:41:55 +0900

    [FEAT] 회원가입 api 구현

--- src/main/java/com/tiki/server/member/service/MemberService.java
+++ src/main/java/com/tiki/server/member/service/MemberService.java
@@ -1,37 +1,38 @@
 package com.tiki.server.member.service;
 
 import com.tiki.server.member.adapter.MemberFinder;
+import com.tiki.server.member.adapter.MemberSaver;
 import com.tiki.server.member.dto.request.MemberProfileCreateRequest;
 import com.tiki.server.member.entity.Member;
 import com.tiki.server.member.exception.MemberException;
-import com.tiki.server.member.message.ErrorCode;
-import com.tiki.server.member.repository.MemberRepository;
 import lombok.NonNull;
 import lombok.val;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.services.s3.model.ObjectPart;
-
-import java.net.http.HttpRequest;
-import java.util.Optional;
 
 import static com.tiki.server.member.message.ErrorCode.CONFLICT_MEMBER;
 import static com.tiki.server.member.message.ErrorCode.UNMATCHED_PASSWORD;
-import static sun.security.timestamp.TSResponse.BAD_REQUEST;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
 
+    MemberSaver memberSaver;
     MemberFinder memberFinder;
 
     public void signUp(@NonNull MemberProfileCreateRequest request) {
         emailCheck(request.email());
         passwordCheck(request.password(), request.passwordCk());
 
+        memberSaver.save(Member.of(
+                request.email(),
+                request.password(),
+                request.name(),
+                request.birth(),
+                request.Univ()));
     }
 
     private void emailCheck(String email) {
