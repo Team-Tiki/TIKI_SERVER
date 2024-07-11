22da79a4129b2ef577a2e4fe27215449fcf09920
paragon0107 <smg0725@naver.com>
2024-07-12 00:41:13 +0900

    [CHORE] 회원가입 Dto birth 데이터 형식 변경

--- src/main/java/com/tiki/server/member/dto/request/MemberProfileCreateRequest.java
+++ src/main/java/com/tiki/server/member/dto/request/MemberProfileCreateRequest.java
@@ -1,13 +1,13 @@
 package com.tiki.server.member.dto.request;
 
-import com.tiki.server.common.entity.School;
+import java.time.LocalDate;
+
 import com.tiki.server.common.entity.University;
-import lombok.Data;
 import lombok.NonNull;
 
 public record MemberProfileCreateRequest(
         @NonNull String name,
-        @NonNull Data birth,
+        @NonNull LocalDate birth,
         @NonNull University Univ,
         @NonNull String email,
         @NonNull String password,
