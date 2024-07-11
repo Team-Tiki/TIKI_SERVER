b7281156cfee929146c2c450eae1d7f7a0e86832
paragon0107 <smg0725@naver.com>
2024-07-11 23:05:51 +0900

    [FEAT] 이메일 존재 체크 메서드 추가

--- src/main/java/com/tiki/server/member/adapter/MemberFinder.java
+++ src/main/java/com/tiki/server/member/adapter/MemberFinder.java
@@ -1,9 +1,15 @@
 package com.tiki.server.member.adapter;
 
 import com.tiki.server.common.support.RepositoryAdapter;
+import com.tiki.server.member.entity.Member;
+import com.tiki.server.member.exception.MemberException;
 import com.tiki.server.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import static com.tiki.server.member.message.ErrorCode.INVALID_MEMBER;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
@@ -11,5 +17,8 @@ public class MemberFinder {
 
 	private final MemberRepository memberRepository;
 
+	public Optional<Member> findMemberByEmail(String email){
+		return memberRepository.findByEmail(email);
+	}
 
 }
