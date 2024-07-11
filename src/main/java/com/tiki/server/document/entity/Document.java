d3bf33ea8ca829f085301b701500861c645c4005
Chan531 <chan657312@gmail.com>
2024-07-11 19:44:40 +0900

    [REFACTOR] Document 컬럼 및 생성 방식 변경

--- src/main/java/com/tiki/server/document/entity/Document.java
+++ src/main/java/com/tiki/server/document/entity/Document.java
@@ -1,19 +1,13 @@
 package com.tiki.server.document.entity;
 
-import static com.tiki.server.document.entity.DocumentStatus.BASIC;
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import com.tiki.server.common.entity.BaseTime;
 import com.tiki.server.timeblock.entity.TimeBlock;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -38,15 +32,11 @@ public class Document extends BaseTime {
 	@JoinColumn(name = "block_id")
 	private TimeBlock timeBlock;
 
-	@Enumerated(value = STRING)
-	private DocumentStatus status;
-
-	private LocalDate deletedDate;
-
 	@Builder
-	public Document(String fileUrl, TimeBlock timeBlock) {
-		this.fileUrl = fileUrl;
-		this.timeBlock = timeBlock;
-		this.status = BASIC;
+	public static Document of(String fileUrl, TimeBlock timeBlock) {
+		return Document.builder()
+			.fileUrl(fileUrl)
+			.timeBlock(timeBlock)
+			.build();
 	}
 }
