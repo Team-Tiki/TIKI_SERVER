03b2e736f79b40b9b73d87f9c4940682cd7e470c
paragon0107 <smg0725@naver.com>
2024-07-12 01:28:40 +0900

    [CHORE] 불필요한 import문 삭제

--- src/main/java/com/tiki/server/member/adapter/MemberFinder.java
+++ src/main/java/com/tiki/server/member/adapter/MemberFinder.java
@@ -1,20 +1,15 @@
 package com.tiki.server.member.adapter;
 
 import static com.tiki.server.member.message.ErrorCode.INVALID_MEMBER;
-import static com.tiki.server.team.message.ErrorCode.INVALID_TEAM;
 
 import com.tiki.server.common.support.RepositoryAdapter;
 import com.tiki.server.member.entity.Member;
 import com.tiki.server.member.exception.MemberException;
 import com.tiki.server.member.repository.MemberRepository;
-import com.tiki.server.team.entity.Team;
-import com.tiki.server.team.exception.TeamException;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
-
-import static com.tiki.server.member.message.ErrorCode.INVALID_MEMBER;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
--- src/main/java/com/tiki/server/member/adapter/MemberSaver.java
+++ src/main/java/com/tiki/server/member/adapter/MemberSaver.java
@@ -2,14 +2,8 @@ package com.tiki.server.member.adapter;
 
 import com.tiki.server.common.support.RepositoryAdapter;
 import com.tiki.server.member.entity.Member;
-import com.tiki.server.member.exception.MemberException;
 import com.tiki.server.member.repository.MemberRepository;
-import com.tiki.server.timeblock.entity.TimeBlock;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Optional;
-
-import static com.tiki.server.member.message.ErrorCode.INVALID_MEMBER;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
