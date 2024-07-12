ad4d1e407297299c294371eefe6fbe2331a54f8f
paragon0107 <smg0725@naver.com>
2024-07-12 16:53:25 +0900

    [FEAT] 메일 컨트롤러 구현

---
+++ src/main/java/com/tiki/server/mail/controller/EmailController.java
@@ -0,0 +1,30 @@
+package com.tiki.server.mail.controller;
+
+import com.tiki.server.common.dto.BaseResponse;
+import com.tiki.server.mail.dto.request.EmailRequestDto;
+import com.tiki.server.mail.service.EmailService;
+import com.tiki.server.mail.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tiki.server.common.dto.SuccessResponse.success;
+import static com.tiki.server.common.support.UriGenerator.getUri;
+import static com.tiki.server.external.message.SuccessMessage.SUCCESS_SEND_MAIL;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/mail")
+public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> sendJoinMail(@RequestBody EmailRequestDto emailRequestDto) {
+        mailService.sendMail(emailRequestDto);
+        return ResponseEntity.created(getUri("/")).body(success(SUCCESS_SEND_MAIL.getMessage()));
+    }
+}
