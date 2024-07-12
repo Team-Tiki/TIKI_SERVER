ad461cce06e83e1d68205f6a33fd20c40fd7b801
Chan531 <chan657312@gmail.com>
2024-07-13 01:30:04 +0900

    [FEAT] 타임 블록 상세 조회

--- src/main/java/com/tiki/server/timeblock/service/TimeBlockService.java
+++ src/main/java/com/tiki/server/timeblock/service/TimeBlockService.java
@@ -5,7 +5,6 @@ import static com.tiki.server.timeblock.message.ErrorCode.INVALID_TYPE;
 import static com.tiki.server.timeblock.constant.TimeBlockConstant.EXECUTIVE;
 import static com.tiki.server.timeblock.constant.TimeBlockConstant.MEMBER;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
@@ -15,7 +14,6 @@ import com.tiki.server.common.entity.Position;
 import com.tiki.server.document.adapter.DocumentFinder;
 import com.tiki.server.document.adapter.DocumentSaver;
 import com.tiki.server.document.entity.Document;
-import com.tiki.server.document.vo.DocumentVO;
 import com.tiki.server.memberteammanager.adapter.MemberTeamManagerFinder;
 import com.tiki.server.team.adapter.TeamFinder;
 import com.tiki.server.team.entity.Team;
@@ -23,10 +21,10 @@ import com.tiki.server.timeblock.adapter.TimeBlockFinder;
 import com.tiki.server.timeblock.adapter.TimeBlockSaver;
 import com.tiki.server.timeblock.dto.request.TimeBlockCreateRequest;
 import com.tiki.server.timeblock.dto.response.TimeBlockCreateResponse;
+import com.tiki.server.timeblock.dto.response.TimeBlockDetailGetResponse;
 import com.tiki.server.timeblock.dto.response.TimelineGetResponse;
 import com.tiki.server.timeblock.entity.TimeBlock;
 import com.tiki.server.timeblock.exception.TimeBlockException;
-import com.tiki.server.timeblock.vo.TimeBlockVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -67,6 +65,14 @@ public class TimeBlockService {
 			case MEMBER -> getTimelineByType(team, Position.MEMBER, position, date);
 			default -> throw new TimeBlockException(INVALID_TYPE);
 		};
+	}
+
+	public TimeBlockDetailGetResponse getTimeBlockDetail(long memberId, long teamId, long timeBlockId) {
+		val position = memberTeamManagerFinder.findByMemberIdAndTeamId(memberId, teamId).getPosition();
+		val timeBlock = timeBlockFinder.findById(timeBlockId);
+		checkMemberAccessible(timeBlock.accessiblePosition(), position);
+		val documents = documentFinder.findAllByTimeBlockId(timeBlockId);
+		return TimeBlockDetailGetResponse.from(documents);
 	}
 
 	private TimeBlockCreateResponse createTimeBlockByType(
