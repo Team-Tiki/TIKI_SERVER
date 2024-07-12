133ae6f1b0a9d2024f9018d0c9816415ce605ce3
Chan531 <chan657312@gmail.com>
2024-07-12 16:33:10 +0900

    [INIT] Cors 설정

--- src/main/java/com/tiki/server/auth/config/SecurityConfig.java
+++ src/main/java/com/tiki/server/auth/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import com.tiki.server.auth.filter.ExceptionHandlerFilter;
 import com.tiki.server.auth.filter.JwtAuthenticationFilter;
 import com.tiki.server.auth.jwt.JwtProvider;
 import com.tiki.server.auth.jwt.JwtValidator;
+import com.tiki.server.common.config.CorsConfig;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +25,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CorsConfig corsConfig;
     private final CustomAuthenticationEntryPointHandler customAuthenticationEntryPointHandler;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -50,6 +53,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/v1/member").permitAll()
                                 .anyRequest()
                                 .authenticated())
+                .addFilter(corsConfig.corsFilter())
                 .addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
                 )
---
+++ src/main/java/com/tiki/server/common/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.tiki.server.common.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.val;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsFilter corsFilter() {
+		val source = setUrlBasedCorsConfigurationSource();
+		return new CorsFilter(source);
+	}
+
+	private UrlBasedCorsConfigurationSource setUrlBasedCorsConfigurationSource() {
+		val source = new UrlBasedCorsConfigurationSource();
+		val config = setCorsConfiguration();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+
+	private CorsConfiguration setCorsConfiguration() {
+		val config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:5173");
+		config.addAllowedHeader("*");
+		config.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		return config;
+	}
+}
