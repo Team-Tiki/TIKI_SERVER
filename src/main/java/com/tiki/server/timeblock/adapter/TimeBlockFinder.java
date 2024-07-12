022cc47c3933a1c825b448c95df0459268362536
Chan531 <chan657312@gmail.com>
2024-07-13 01:29:48 +0900

    [FEAT] TimeBlock 찾기

--- src/main/java/com/tiki/server/timeblock/adapter/TimeBlockFinder.java
+++ src/main/java/com/tiki/server/timeblock/adapter/TimeBlockFinder.java
@@ -1,10 +1,14 @@
 package com.tiki.server.timeblock.adapter;
 
+import static com.tiki.server.timeblock.message.ErrorCode.*;
+
 import java.util.List;
 
 import com.tiki.server.common.entity.Position;
 import com.tiki.server.common.support.RepositoryAdapter;
 import com.tiki.server.team.entity.Team;
+import com.tiki.server.timeblock.exception.TimeBlockException;
+import com.tiki.server.timeblock.message.ErrorCode;
 import com.tiki.server.timeblock.repository.TimeBlockRepository;
 import com.tiki.server.timeblock.vo.TimeBlockVO;
 
@@ -15,6 +19,12 @@ import lombok.RequiredArgsConstructor;
 public class TimeBlockFinder {
 
 	private final TimeBlockRepository timeBlockRepository;
+
+	public TimeBlockVO findById(long id) {
+		return timeBlockRepository.findById(id)
+			.map(TimeBlockVO::from)
+			.orElseThrow(() -> new TimeBlockException(INVALID_TIME_BLOCK));
+	}
 
 	public List<TimeBlockVO> findByTeamAndAccessiblePositionAndDate(
 		long teamId,
