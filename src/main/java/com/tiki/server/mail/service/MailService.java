91606897e59375ca164252d96eabba5caa69f492
paragon0107 <smg0725@naver.com>
2024-07-15 00:59:24 +0900

    [FEAT] MailService 추가

--- src/main/java/com/tiki/server/mail/service/MailService.java
+++ src/main/java/com/tiki/server/mail/service/MailService.java
@@ -1,115 +1,127 @@
 package com.tiki.server.mail.service;
 
-import com.tiki.server.mail.dto.request.EmailRequestDto;
+import com.tiki.server.mail.adapter.MailFinder;
+import com.tiki.server.mail.adapter.MailSaver;
+import com.tiki.server.mail.dto.request.CodeCheckDto;
+import com.tiki.server.mail.dto.request.MailRequestDto;
+import com.tiki.server.mail.entity.Mail;
+import com.tiki.server.mail.exception.MailException;
+import com.tiki.server.member.adapter.MemberFinder;
 import com.tiki.server.member.exception.MemberException;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import javax.naming.Context;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static com.tiki.server.mail.message.ErrorCode.INVALID_MATCHED;
+import static com.tiki.server.mail.message.ErrorCode.INVALID_REQUEST;
+import static com.tiki.server.member.message.ErrorCode.*;
+import static com.tiki.server.mail.constants.MailConstants.*;
+
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MailService {
-    private final JavaMailSender mailSender;
-    private final RedisUtil redisUtil;
 
+    private final SpringTemplateEngine templateEngine;
+    private final JavaMailSender javaMailSender;
+    private final MemberFinder memberFinder;
+    private final MailFinder mailFinder;
+    private final MailSaver mailSaver;
 
-
-    @Value("${spring.mail.username}")
-    private String configEmail;
-
-    // 메일 보내기
-    public void sendMail(EmailRequestDto emailRequestDto) throws MessagingException {
-        checkMailFormat(emailRequestDto);
-//        if (redisUtil.existData(toEmail)) {
-//            redisUtil.deleteData(toEmail);
-//        }
-//        MimeMessage emailForm = createEmailForm(toEmail);
-//
-//        mailSender.send(emailForm);
+    @Transactional
+    public void sendSignUp(MailRequestDto mailRequestDto) {
+        val address = mailRequestDto.address();
+        checkSignUpEmail(address);
+        processingMail(address, MAIL_SUBJECT_SIGN_UP);
+    }
+    @Transactional
+    public void sendChangingPassword(MailRequestDto mailRequestDto) {
+        val address = mailRequestDto.address();
+        checkPasswordEmail(address);
+        processingMail(address, MAIL_SUBJECT_CHANGING_PASSWORD);
     }
 
-    private void checkMailFormat(EmailRequestDto emailRequestDto) {
-        val address = emailRequestDto.getEmail();
-        if(!(address.contains("@") && !address.endsWith(".edu") && address.endsWith(".ac.kr") && address.endsWith("academy "))){
-            throw new MemberException()
+    public void checkCode(CodeCheckDto codeCheckDto) {
+        val address = codeCheckDto.address();
+        checkEmailFormat(address);
+        val mail = mailFinder.findById(address);
+        if (mail.isEmpty()) {
+            throw new MailException(INVALID_REQUEST);
+        }
+        if (!mail.get().getCode().equals(codeCheckDto.code())) {
+            throw new MailException(INVALID_MATCHED);
         }
     }
 
-    private String createdCode() {
-        int leftLimit = 48; // number '0'
-        int rightLimit = 122; // alphabet 'z'
-        int targetStringLength = 6;
-        Random random = new Random();
-
-        return random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <=57 || i >=65) && (i <= 90 || i>= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
+    private void checkSignUpEmail(String address) {
+        memberFinder.findByEmail(address).ifPresent(member -> {
+            throw new MemberException(CONFLICT_MEMBER);
+        });
+        checkEmailFormat(address);
     }
 
-    private String setContext(String code) {
-        Context context = new Context();
-        TemplateEngine templateEngine = new TemplateEngine();
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-
-        context.setVariable("code", code);
-
-
-        templateResolver.setPrefix("templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(false);
-
-        templateEngine.setTemplateResolver(templateResolver);
-
-        return templateEngine.process("mail", context);
+    private void checkPasswordEmail(String address) {
+        if (memberFinder.findByEmail(address).isEmpty()) {
+            throw new MemberException(INVALID_MEMBER);
+        }
+        checkEmailFormat(address);
     }
 
+    private void processingMail(String address, String subject) {
+        val code = generateRandomValue();
+        val message = makeMessage(address, code, subject);
+        mailSaver.save(Mail.of(address, code));
+        send(message);
+    }
 
-    // 메일 반환
+    private void checkEmailFormat(String address) {
+        if (!(address.endsWith(MAIL_FORMAT_EDU) || address.endsWith(MAIL_FORMAT_AC_KR))) {
+            throw new MemberException(INVALID_EMAIL);
+        }
+    }
 
-    private MimeMessage createEmailForm(String email) throws MessagingException {
-
-        String authCode = createdCode();
-
-        MimeMessage message = mailSender.createMimeMessage();
-        message.addRecipients(MimeMessage.RecipientType.TO, email);
-        message.setSubject("안녕하세요 인증번호입니다.");
-        message.setFrom(configEmail);
-        message.setText(setContext(authCode), "utf-8", "html");
-
-        redisUtil.setDataExpire(email, authCode, 60 * 30L);
-
+    private MimeMessage makeMessage(String address, String code, String subject) {
+        val message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(TIKI_ADDRESS);
+            helper.setTo(address);
+            helper.setSubject(subject);
+            helper.setText(setContext(code), true);
+            helper.addInline("image", new ClassPathResource("images/mail_logo.png"));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
         return message;
     }
 
-    // 코드 검증
-    public Boolean verifyEmailCode(String email, String code) {
-        String codeFoundByEmail = redisUtil.getData(email);
-        System.out.println(codeFoundByEmail);
-        if (codeFoundByEmail == null) {
-            return false;
-        }
-        return codeFoundByEmail.equals(code);
+    private void send(MimeMessage email) {
+        javaMailSender.send(email);
     }
 
-    public String makeMemberId(String email) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(email.getBytes());
-        md.update(LocalDateTime.now().toString().getBytes());
-        return Arrays.toString(md.digest());
+    public static String generateRandomValue() {
+        Random random = new Random();
+
+        return IntStream.range(0, 6).mapToObj(i -> String.valueOf(random.nextInt(10))).collect(Collectors.joining());
     }
 
+    public String setContext(String code) {
+        Context context = new Context();
+        context.setVariable("code", code);
+        return templateEngine.process("certification", context);
+    }
 }
