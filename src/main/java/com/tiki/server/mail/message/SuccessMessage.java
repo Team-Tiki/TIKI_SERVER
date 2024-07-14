ad8d1560dd90405faf91056dcd93ae423f903f6c
paragon0107 <smg0725@naver.com>
2024-07-15 01:00:14 +0900

    [FEAT] 메일 SuccessMessage 추가

--- src/main/java/com/tiki/server/mail/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/mail/message/SuccessMessage.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessMessage {
 
-	SUCCESS_SEND_EMAIL("이메일 전송 성공");
+    SUCCESS_SEND_EMAIL("이메일 전송 성공"),
+    SUCCESS_VALIDATION("인증 성공");
 
-	private final String message;
+    private final String message;
 }
