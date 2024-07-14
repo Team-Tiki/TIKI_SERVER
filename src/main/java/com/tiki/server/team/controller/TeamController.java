10a4d08d883c8307b57d1297a978d9b4332e4000
Chan531 <chan657312@gmail.com>
2024-07-14 20:02:52 +0900

    [FEAT] 팀 생성

--- src/main/java/com/tiki/server/team/controller/TeamController.java
+++ src/main/java/com/tiki/server/team/controller/TeamController.java
@@ -1,17 +1,44 @@
 package com.tiki.server.team.controller;
 
+import static com.tiki.server.team.message.SuccessMessage.SUCCESS_CREATE_TEAM;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tiki.server.common.dto.BaseResponse;
+import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.common.support.UriGenerator;
+import com.tiki.server.team.controller.docs.TeamControllerDocs;
+import com.tiki.server.team.dto.request.TeamCreateRequest;
+import com.tiki.server.team.dto.response.TeamCreateResponse;
 import com.tiki.server.team.service.TeamService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/teams")
-public class TeamController {
+public class TeamController implements TeamControllerDocs {
 
 	private final TeamService teamService;
 
+	@Override
+	@PostMapping
+	public ResponseEntity<SuccessResponse<TeamCreateResponse>> createTeam(
+		Principal principal,
+		@RequestBody TeamCreateRequest request
+	) {
+		val memberId = Long.parseLong(principal.getName());
+		val response = teamService.createTeam(memberId, request);
+		return ResponseEntity.created(
+			UriGenerator.getUri("/api/v1/teams/" + response.teamId())
+		).body(SuccessResponse.success(SUCCESS_CREATE_TEAM.getMessage(), response));
+	}
 }
--- src/main/java/com/tiki/server/team/service/TeamService.java
+++ src/main/java/com/tiki/server/team/service/TeamService.java
@@ -1,12 +1,47 @@
 package com.tiki.server.team.service;
 
+import static com.tiki.server.common.entity.Position.ADMIN;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tiki.server.common.entity.Position;
+import com.tiki.server.common.entity.University;
+import com.tiki.server.member.adapter.MemberFinder;
+import com.tiki.server.member.entity.Member;
+import com.tiki.server.memberteammanager.adapter.MemberTeamManagerSaver;
+import com.tiki.server.memberteammanager.entity.MemberTeamManager;
+import com.tiki.server.team.adapter.TeamSaver;
+import com.tiki.server.team.dto.request.TeamCreateRequest;
+import com.tiki.server.team.dto.response.TeamCreateResponse;
+import com.tiki.server.team.entity.Team;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamService {
+
+	private final TeamSaver teamSaver;
+	private final MemberFinder memberFinder;
+	private final MemberTeamManagerSaver memberTeamManagerSaver;
+
+	@Transactional
+	public TeamCreateResponse createTeam(long memberId, TeamCreateRequest request) {
+		val member = memberFinder.findById(memberId);
+		val team = teamSaver.save(createTeam(request, member.getUniv()));
+		memberTeamManagerSaver.save(createMemberTeamManager(member, team, ADMIN));
+		return TeamCreateResponse.from(team);
+	}
+
+	private Team createTeam(TeamCreateRequest request, University univ) {
+		return Team.of(request, univ);
+	}
+
+	private MemberTeamManager createMemberTeamManager(Member member, Team team, Position position) {
+		return MemberTeamManager.of(member, team, position);
+	}
 }
