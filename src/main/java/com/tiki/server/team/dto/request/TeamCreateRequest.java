f7c2d592973b938f301fb4b833051bd699e864c2
Chan531 <chan657312@gmail.com>
2024-07-14 20:00:55 +0900

    [ADD] 팀 생성 dto 추가

---
+++ src/main/java/com/tiki/server/team/dto/request/TeamCreateRequest.java
@@ -0,0 +1,12 @@
+package com.tiki.server.team.dto.request;
+
+import com.tiki.server.team.entity.Category;
+
+import lombok.NonNull;
+
+public record TeamCreateRequest(
+	@NonNull String name,
+	@NonNull Category category,
+	@NonNull String iconImageUrl
+) {
+}
---
+++ src/main/java/com/tiki/server/team/dto/response/TeamCreateResponse.java
@@ -0,0 +1,19 @@
+package com.tiki.server.team.dto.response;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import com.tiki.server.team.entity.Team;
+
+import lombok.Builder;
+
+@Builder(access = PRIVATE)
+public record TeamCreateResponse(
+	long teamId
+) {
+
+	public static TeamCreateResponse from(Team team) {
+		return TeamCreateResponse.builder()
+			.teamId(team.getId())
+			.build();
+	}
+}
