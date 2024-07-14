3dcbd93b557cf288ae3bdaae70e09a8c8edc1e32
paragon0107 <smg0725@naver.com>
2024-07-15 00:58:22 +0900

    [FEAT] MailException 추가

--- src/main/java/com/tiki/server/mail/exception/MailException.java
+++ src/main/java/com/tiki/server/mail/exception/MailException.java
@@ -1,12 +1,12 @@
 package com.tiki.server.mail.exception;
 
-import com.tiki.server.member.message.ErrorCode;
+import com.tiki.server.mail.message.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class MailException extends RuntimeException {
 
-	private final ErrorCode errorCode;
+    private final ErrorCode errorCode;
 
     public MailException(ErrorCode errorCode) {
         super("[MailException] : " + errorCode.getMessage());
