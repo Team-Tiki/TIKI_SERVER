7b0137434439302fc99d12315391aa18d41ac013
Chan531 <chan657312@gmail.com>
2024-07-14 20:03:03 +0900

    [ADD] 팀 생성 스웨거 추가

---
+++ src/main/java/com/tiki/server/team/controller/docs/TeamControllerDocs.java
@@ -0,0 +1,53 @@
+package com.tiki.server.team.controller.docs;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tiki.server.common.dto.ErrorResponse;
+import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.team.dto.request.TeamCreateRequest;
+import com.tiki.server.team.dto.response.TeamCreateResponse;
+import com.tiki.server.timeblock.dto.request.TimeBlockCreateRequest;
+import com.tiki.server.timeblock.dto.response.TimeBlockCreateResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "teams", description = "팀 API")
+public interface TeamControllerDocs {
+
+	@Operation(
+		summary = "팀 생성",
+		description = "팀을 생성한다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "201",
+				description = "성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+			@ApiResponse(
+				responseCode = "404",
+				description = "유효하지 않은 회원",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "4xx",
+				description = "클라이언트(요청) 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "500",
+				description = "서버 내부 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+	)
+	ResponseEntity<SuccessResponse<TeamCreateResponse>> createTeam(
+		@Parameter(hidden = true) Principal principal,
+		@RequestBody TeamCreateRequest request
+	);
+}
