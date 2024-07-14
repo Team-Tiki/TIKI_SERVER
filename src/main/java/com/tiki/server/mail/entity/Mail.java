e181030b9cfd0ebda9850ee260dd80a06526c0cd
paragon0107 <smg0725@naver.com>
2024-07-15 00:56:58 +0900

    [FEAT] MAIL 엔티티 추가

--- src/main/java/com/tiki/server/mail/entity/Mail.java
+++ src/main/java/com/tiki/server/mail/entity/Mail.java
@@ -1,4 +1,24 @@
 package com.tiki.server.mail.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@AllArgsConstructor
+@Getter
+@RedisHash(value = "mailVerification", timeToLive = 180)
 public class Mail {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+    private String code;
+
+    public static Mail of(String address, String code) {
+        return new Mail(address, code);
+    }
 }
