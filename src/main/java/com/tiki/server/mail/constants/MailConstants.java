fd048826b307deb47b4ce6bd34b12ad35e8461ab
paragon0107 <smg0725@naver.com>
2024-07-15 03:44:15 +0900

    [CHORE] static 파일 경로, 상수로 변경

--- src/main/java/com/tiki/server/mail/constants/MailConstants.java
+++ src/main/java/com/tiki/server/mail/constants/MailConstants.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class MailConstants {
 
-    @Value("${mail.password}")
-    public static String MailPassword;
     public static final String TIKI_ADDRESS = "hello.wer.tiki@gmail.com";
     public static final String MAIL_SUBJECT_SIGN_UP = "[Ti.Ki] 회원가입: 이메일 인증번호 안내";
     public static final String MAIL_SUBJECT_CHANGING_PASSWORD = "[Ti.Ki] 회원가입: 이메일 인증번호 안내";
+    public static final String IMG_PATH = "images/mail_logo.png";
     public static final String MAIL_FORMAT_EDU = ".edu";
     public static final String MAIL_FORMAT_AC_KR = ".ac.kr";
+    public static final String TEMPLATE_NAME = "certification";
 }
