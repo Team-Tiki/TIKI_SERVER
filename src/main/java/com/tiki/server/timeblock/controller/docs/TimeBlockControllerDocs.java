2dc6ec073b08eb145d4d0dc706c9c874c961e263
Chan531 <chan657312@gmail.com>
2024-07-14 17:02:25 +0900

    [ADD] 타임 블록 상세 조회 api Docs에 추가

--- src/main/java/com/tiki/server/timeblock/controller/docs/TimeBlockControllerDocs.java
+++ src/main/java/com/tiki/server/timeblock/controller/docs/TimeBlockControllerDocs.java
@@ -11,6 +11,7 @@ import com.tiki.server.common.dto.ErrorResponse;
 import com.tiki.server.common.dto.SuccessResponse;
 import com.tiki.server.timeblock.dto.request.TimeBlockCreateRequest;
 import com.tiki.server.timeblock.dto.response.TimeBlockCreateResponse;
+import com.tiki.server.timeblock.dto.response.TimeBlockDetailGetResponse;
 import com.tiki.server.timeblock.dto.response.TimelineGetResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -129,5 +130,52 @@ public interface TimeBlockControllerDocs {
 			in = ParameterIn.QUERY,
 			example = "2024-07"
 		) @RequestParam String date
+	);
+
+	@Operation(
+		summary = "타임라인 조회",
+		description = "타임라인을 조회한다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+			@ApiResponse(
+				responseCode = "403",
+				description = "접근 권한 없음",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "404",
+				description = "팀에 존재하지 않는 회원",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "404",
+				description = "유효하지 않은 타임 블록",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "4xx",
+				description = "클라이언트(요청) 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "500",
+				description = "서버 내부 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+	)
+	ResponseEntity<SuccessResponse<TimeBlockDetailGetResponse>> getTimeBlockDetail(
+		@Parameter(hidden = true) Principal principal,
+		@Parameter(
+			name = "teamId",
+			description = "팀 id",
+			in = ParameterIn.PATH,
+			example = "1"
+		)
+		@PathVariable long teamId,
+		@Parameter(
+			name = "timeBlockId",
+			description = "타임 블록 id",
+			in = ParameterIn.PATH,
+			example = "1"
+		)
+		@PathVariable long timeBlockId
 	);
 }
