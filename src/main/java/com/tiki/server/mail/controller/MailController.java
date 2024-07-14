d61b5d28276717257928748145413e047428175c
paragon0107 <smg0725@naver.com>
2024-07-15 00:58:04 +0900

    [FEAT] MailController 추가

--- src/main/java/com/tiki/server/mail/controller/MailController.java
+++ src/main/java/com/tiki/server/mail/controller/MailController.java
@@ -1,10 +1,10 @@
 package com.tiki.server.mail.controller;
 
 import com.tiki.server.common.dto.BaseResponse;
-import com.tiki.server.mail.dto.request.EmailRequestDto;
-import com.tiki.server.mail.service.EmailService;
+
+import com.tiki.server.mail.dto.request.CodeCheckDto;
+import com.tiki.server.mail.dto.request.MailRequestDto;
 import com.tiki.server.mail.service.MailService;
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.tiki.server.common.dto.SuccessResponse.success;
 import static com.tiki.server.common.support.UriGenerator.getUri;
+import static com.tiki.server.mail.constants.MailConstants.MAIL_SUBJECT_CHANGING_PASSWORD;
+import static com.tiki.server.mail.constants.MailConstants.MAIL_SUBJECT_SIGN_UP;
 import static com.tiki.server.mail.message.SuccessMessage.SUCCESS_SEND_EMAIL;
+import static com.tiki.server.mail.message.SuccessMessage.SUCCESS_VALIDATION;
 
 
 @RestController
@@ -24,9 +27,22 @@ public class MailController {
 
     private final MailService mailService;
 
-    @PostMapping
-    public ResponseEntity<BaseResponse> sendJoinMail(@RequestBody EmailRequestDto emailRequestDto) throws MessagingException {
-        mailService.sendMail(emailRequestDto);
+    @PostMapping("/signup")
+    public ResponseEntity<BaseResponse> sendSignUpMail(@RequestBody MailRequestDto mailRequestDto) {
+        mailService.sendSignUp(mailRequestDto);
         return ResponseEntity.created(getUri("/")).body(success(SUCCESS_SEND_EMAIL.getMessage()));
     }
+
+    @PostMapping("/password")
+    public ResponseEntity<BaseResponse> sendChangingPasswordMail(@RequestBody MailRequestDto mailRequestDto) {
+        mailService.sendChangingPassword(mailRequestDto);
+        return ResponseEntity.created(getUri("/")).body(success(SUCCESS_SEND_EMAIL.getMessage()));
+    }
+
+    @PostMapping("/checking")
+    public ResponseEntity<BaseResponse> checkCode(@RequestBody CodeCheckDto codeCheckDto) {
+        mailService.checkCode(codeCheckDto);
+        return ResponseEntity.created(getUri("/")).body(success(SUCCESS_VALIDATION.getMessage()));
+    }
+
 }
