656b62f317b2c1e0d18e79bf6499ef29242cb353
Chan531 <chan657312@gmail.com>
2024-07-15 04:25:26 +0900

    [ADD] s3 파일 삭제 스웨거 적용

--- src/main/java/com/tiki/server/external/controller/docs/S3ControllerDocs.java
+++ src/main/java/com/tiki/server/external/controller/docs/S3ControllerDocs.java
@@ -4,9 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tiki.server.common.dto.BaseResponse;
 import com.tiki.server.common.dto.ErrorResponse;
 import com.tiki.server.common.dto.SuccessResponse;
-import com.tiki.server.external.dto.request.PreSignedUrlRequest;
+import com.tiki.server.external.dto.request.S3DeleteRequest;
 import com.tiki.server.external.dto.response.PreSignedUrlResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -44,5 +45,26 @@ public interface S3ControllerDocs {
 			in = ParameterIn.QUERY,
 			example = "hwp, pdf, ..."
 		) @RequestParam String fileFormat
+	);
+
+	@Operation(
+		summary = "Presigned Url 생성",
+		description = "s3로부터 Presigned Url을 생성한다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+			@ApiResponse(
+				responseCode = "4xx",
+				description = "클라이언트(요청) 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "500",
+				description = "S3 PRESIGNED URL 불러오기 실패",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+	)
+	ResponseEntity<BaseResponse> deleteFile(
+		@RequestBody S3DeleteRequest request
 	);
 }
