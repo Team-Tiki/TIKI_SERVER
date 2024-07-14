a60aa794212c3e9864838ae671d829c02d3c3e7b
paragon0107 <smg0725@naver.com>
2024-07-15 03:42:50 +0900

    [CHORE] 컨벤션 적용

--- src/main/java/com/tiki/server/mail/entity/Mail.java
+++ src/main/java/com/tiki/server/mail/entity/Mail.java
@@ -3,22 +3,26 @@ package com.tiki.server.mail.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PRIVATE;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor(access = PRIVATE)
+@Builder
 @RedisHash(value = "mailVerification", timeToLive = 180)
 public class Mail {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private String id;
+
     private String code;
 
     public static Mail of(String address, String code) {
-        return new Mail(address, code);
+        return Mail.builder().id(address).code(code).build();
     }
 }
--- src/main/java/com/tiki/server/member/message/ErrorCode.java
+++ src/main/java/com/tiki/server/member/message/ErrorCode.java
@@ -11,14 +11,14 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    /* 400 BAD REQUEST: 잘못된 요청*/
+    /* 400 BAD REQUEST: 잘못된 요청 */
     UNMATCHED_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     INVALID_EMAIL(BAD_REQUEST, "잘못된 이메일 형식입니다."),
 
     /* 404 NOT_FOUND : 자원을 찾을 수 없음 */
     INVALID_MEMBER(NOT_FOUND, "유효하지 않은 회원입니다."),
 
-    /* 409 CONFLICT : 중복된 데이터 존재*/
+    /* 409 CONFLICT : 중복된 데이터 존재 */
     CONFLICT_MEMBER(CONFLICT, "존재하는 이메일입니다.");
 
     private final HttpStatus httpStatus;
