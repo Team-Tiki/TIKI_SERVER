bb7cbe2e4376e6316846c6e32673a959cb960cf8
paragon0107 <smg0725@naver.com>
2024-07-16 03:01:46 +0900

    [FEAT] TeamVo로 변경

--- src/main/java/com/tiki/server/team/adapter/TeamFinder.java
+++ src/main/java/com/tiki/server/team/adapter/TeamFinder.java
@@ -8,22 +8,31 @@ import com.tiki.server.team.entity.Team;
 import com.tiki.server.team.exception.TeamException;
 import com.tiki.server.team.repository.TeamRepository;
 
+import com.tiki.server.team.vo.TeamVO;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
 public class TeamFinder {
 
-	private final TeamRepository teamRepository;
+    private final TeamRepository teamRepository;
 
-	public Team findById(long teamId) {
-		return teamRepository.findById(teamId)
-			.orElseThrow(() -> new TeamException(INVALID_TEAM));
-	}
-	public List<Team> findAllByUniv(University univ){
-		return teamRepository.findAllByUniv(univ)
-				.orElseThrow(() -> new TeamException(INVALID_TEAM));
-	}
+    public Team findById(long teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(() -> new TeamException(INVALID_TEAM));
+    }
+
+    public List<TeamVO> findAllByUniv(University univ) {
+        return teamRepository.findAllByUniv(univ)
+                .map(teams -> teams.stream()
+                        .map(TeamVO::from)
+                        .collect(Collectors.toList()))
+                .orElse(Collections.emptyList());
+    }
+
 }
