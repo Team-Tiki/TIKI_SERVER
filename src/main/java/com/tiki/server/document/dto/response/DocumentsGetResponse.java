7035d6a2cbf155e99c8c277df2656a6e715f9fcd
Chan531 <chan657312@gmail.com>
2024-07-16 02:29:59 +0900

    [ADD] 전체 문서 조회 dto 추가

--- src/main/java/com/tiki/server/document/dto/response/DocumentsGetResponse.java
+++ src/main/java/com/tiki/server/document/dto/response/DocumentsGetResponse.java
@@ -1,4 +1,38 @@
 package com.tiki.server.document.dto.response;
 
-public record DocumentsGetResponse() {
+import static lombok.AccessLevel.PRIVATE;
+
+import java.util.List;
+
+import com.tiki.server.document.vo.DocumentVO;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder(access = PRIVATE)
+public record DocumentsGetResponse(
+	List<DocumentGetResponse> documents
+) {
+
+	public static DocumentsGetResponse of(List<DocumentVO> documents) {
+		return DocumentsGetResponse.builder()
+			.documents(documents.stream().map(DocumentGetResponse::from).toList())
+			.build();
+	}
+
+	@Builder(access = PRIVATE)
+	private record DocumentGetResponse(
+		long documentId,
+		@NonNull String fileName,
+		@NonNull String fileUrl
+	) {
+
+		public static DocumentGetResponse from(DocumentVO document) {
+			return DocumentGetResponse.builder()
+				.documentId(document.documentId())
+				.fileName(document.fileName())
+				.fileUrl(document.fileUrl())
+				.build();
+		}
+	}
 }
