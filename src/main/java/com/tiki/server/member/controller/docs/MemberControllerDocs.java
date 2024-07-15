b6135c041f0e1b4c3da80c3294e2b2b96911453d
paragon0107 <smg0725@naver.com>
2024-07-15 16:49:56 +0900

    [FEAT] MemberController 스웨거 설정

---
+++ src/main/java/com/tiki/server/member/controller/docs/MemberControllerDocs.java
@@ -0,0 +1,46 @@
+package com.tiki.server.member.controller.docs;
+
+import com.tiki.server.common.dto.BaseResponse;
+import com.tiki.server.common.dto.ErrorResponse;
+import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.member.dto.request.MemberProfileCreateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface MemberControllerDocs {
+
+    @Operation(
+            summary = "회원가입 API",
+            description = "회원가입을 위한 정보를 보낸다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "회원가입 성공",
+                            content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "이메일 형식 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "비밀번호 불일치",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "이미 가입된 아이디",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+    )
+    public ResponseEntity<BaseResponse> signUp(@RequestBody MemberProfileCreateRequest request);
+}
