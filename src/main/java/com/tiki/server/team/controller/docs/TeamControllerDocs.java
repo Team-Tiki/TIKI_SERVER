a109d3bc8564b30aa8b937b16e9d160420572bad
paragon0107 <smg0725@naver.com>
2024-07-16 04:51:22 +0900

    [FEAT] showAllTeam 스웨거 설정

--- src/main/java/com/tiki/server/team/controller/docs/TeamControllerDocs.java
+++ src/main/java/com/tiki/server/team/controller/docs/TeamControllerDocs.java
@@ -2,6 +2,7 @@ package com.tiki.server.team.controller.docs;
 
 import java.security.Principal;
 
+import com.tiki.server.team.dto.response.TeamsGetResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -45,4 +46,27 @@ public interface TeamControllerDocs {
             @Parameter(hidden = true) Principal principal,
             @RequestBody TeamCreateRequest request
     );
+
+    @Operation(
+            summary = "전체 팀 조회",
+            description = "가입한 대학의 전체 팀을 조회한다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "성공",
+                            content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "유효하지 않은 회원",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+    )
+    public ResponseEntity<SuccessResponse<TeamsGetResponse>> showAllTeam(Principal principal);
 }
