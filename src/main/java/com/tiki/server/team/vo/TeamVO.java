f5be101c3da13815d92851d5afcc79959583a853
paragon0107 <smg0725@naver.com>
2024-07-16 03:01:22 +0900

    [FEAT] TeamVo 생성

---
+++ src/main/java/com/tiki/server/team/vo/TeamVO.java
@@ -0,0 +1,28 @@
+package com.tiki.server.team.vo;
+
+import com.tiki.server.common.entity.University;
+import com.tiki.server.team.entity.Category;
+import com.tiki.server.team.entity.Team;
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+public record TeamVO(
+        long teamId,
+        @NonNull String name,
+        @NonNull Category category,
+        @NonNull University univ,
+        String overview,
+        String imageUrl
+) {
+    public static TeamVO from(Team team) {
+        return TeamVO
+                .builder()
+                .teamId(team.getId())
+                .name(team.getName())
+                .overview(team.getOverview())
+                .category(team.getCategory())
+                .univ(team.getUniv())
+                .build();
+    }
+}
