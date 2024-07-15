e1cb4094844ba75e05815359b15b6bb11ccab2f3
Chan531 <chan657312@gmail.com>
2024-07-16 02:29:42 +0900

    [ADD] 전체 문서 조회 관련 메세지 추가

--- src/main/java/com/tiki/server/document/message/ErrorCode.java
+++ src/main/java/com/tiki/server/document/message/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.tiki.server.document.message;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -11,6 +12,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
+	/* 400 BAD_REQUEST : 잘못된 요청 */
+	INVALID_TYPE(BAD_REQUEST, "유효한 타입이 아닙니다."),
 
 	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
 	INVALID_DOCUMENT(NOT_FOUND, "유효하지 않은 문서입니다."),
