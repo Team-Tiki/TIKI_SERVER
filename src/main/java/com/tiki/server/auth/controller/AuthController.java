fc2df99e839c1990fad3de65fe986a7e7cd6ffe5
paragon0107 <smg0725@naver.com>
2024-07-17 02:32:02 +0900

    [FEAT] 로그인, 리이슈 컨트롤러 구현

--- src/main/java/com/tiki/server/auth/controller/AuthController.java
+++ src/main/java/com/tiki/server/auth/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.tiki.server.auth.controller;
 
 import com.tiki.server.auth.dto.request.LoginRequest;
-import com.tiki.server.auth.dto.response.LoginResponse;
+import com.tiki.server.auth.dto.response.UserAllTokenGetResponse;
+import com.tiki.server.auth.dto.response.UserTokenGetResponse;
 import com.tiki.server.common.dto.SuccessResponse;
 import com.tiki.server.common.support.UriGenerator;
-import com.tiki.server.team.dto.response.TeamCreateResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +15,8 @@ import com.tiki.server.auth.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
+import static com.tiki.server.auth.message.SuccessMessage.SUCCESS_GENERATE_ACCESS_TOKEN;
 import static com.tiki.server.auth.message.SuccessMessage.SUCCESS_SIGN_IN;
-import static com.tiki.server.team.message.SuccessMessage.SUCCESS_CREATE_TEAM;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,9 +31,18 @@ public class AuthController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<SuccessResponse<LoginResponse>> login(@RequestBody LoginRequest request){
-		val response = authService.login(request);
+	public ResponseEntity<SuccessResponse<UserAllTokenGetResponse>> login(
+			HttpServletResponse response,
+			@RequestBody LoginRequest userInfo){
+		val token = authService.login(userInfo,response);
 		return ResponseEntity.created(UriGenerator.getUri("/"))
-				.body(SuccessResponse.success(SUCCESS_SIGN_IN.getMessage(), response));
+				.body(SuccessResponse.success(SUCCESS_SIGN_IN.getMessage(), token));
+	}
+
+	@GetMapping("/reissue")
+	public ResponseEntity<SuccessResponse<UserTokenGetResponse>> reissue(HttpServletRequest request){
+		val response = authService.reissue(request);
+		return ResponseEntity.created(UriGenerator.getUri("/"))
+				.body(SuccessResponse.success(SUCCESS_GENERATE_ACCESS_TOKEN.getMessage(), response));
 	}
 }
