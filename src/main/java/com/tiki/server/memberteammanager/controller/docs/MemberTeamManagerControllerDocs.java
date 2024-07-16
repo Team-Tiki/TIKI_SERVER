a0d8eddaec81e8d086f1740636ae515303b539ae
paragon0107 <smg0725@naver.com>
2024-07-16 22:58:13 +0900

    [CHORE] MemberTeamManagerController 삭제

--- src/main/java/com/tiki/server/memberteammanager/controller/docs/MemberTeamManagerControllerDocs.java
+++
@@ -1,7 +0,0 @@
-package com.tiki.server.memberteammanager.controller.docs;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@Tag(name = "MemberTeamManager", description = "멤버 팀 컨트롤 API")
-public interface MemberTeamManagerControllerDocs {
-}
