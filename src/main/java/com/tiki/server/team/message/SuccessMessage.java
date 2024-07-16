475744938591149835989c7bc8bd8f3ce98f3322
Chan531 <chan657312@gmail.com>
2024-07-17 01:13:07 +0900

    [ADD] 카테고리 조회 메세지

--- src/main/java/com/tiki/server/team/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/team/message/SuccessMessage.java
@@ -9,6 +9,7 @@ public enum SuccessMessage {
 
 	SUCCESS_CREATE_TEAM("팀 생성 성공"),
 	SUCCESS_GET_TEAMS("전체 팀 불러오기 성공"),
+	SUCCESS_GET_CATEGORIES("카테고리 리스트 불러오기 성공"),
 	SUCCESS_GET_JOINED_TEAM("소속 팀 불러오기 성공");
 
 	private final String message;
