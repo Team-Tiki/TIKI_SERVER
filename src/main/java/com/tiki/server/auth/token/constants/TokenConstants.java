3adef2d611ae7dddc50c2cd888933a1fff125118
paragon0107 <smg0725@naver.com>
2024-07-17 02:37:46 +0900

    [FEAT] 리프레쉬 토큰 만료시간 추가

---
+++ src/main/java/com/tiki/server/auth/token/constants/TokenConstants.java
@@ -0,0 +1,10 @@
+package com.tiki.server.auth.token.constants;
+
+import org.springframework.beans.factory.annotation.Value;
+
+
+public class TokenConstants {
+
+    @Value("${jwt.refresh-token-expire-time}")
+    public static int REFRESH_TOKEN_EXPIRED_TIME;
+}
