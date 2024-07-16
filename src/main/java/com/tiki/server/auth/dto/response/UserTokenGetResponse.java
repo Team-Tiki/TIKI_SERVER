676d56d80a80c3e680dc91450639878d69895f0a
paragon0107 <smg0725@naver.com>
2024-07-17 02:39:29 +0900

    [FEAT] UserTokenGetResponse 추가

---
+++ src/main/java/com/tiki/server/auth/dto/response/UserTokenGetResponse.java
@@ -0,0 +1,15 @@
+package com.tiki.server.auth.dto.response;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Builder(access = PRIVATE)
+public record UserTokenGetResponse(
+        @NonNull String accessToken
+) {
+    public static UserTokenGetResponse from(String accessToken){
+        return UserTokenGetResponse.builder().accessToken(accessToken).build();
+    }
+}
