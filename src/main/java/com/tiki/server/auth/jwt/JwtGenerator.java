ff257a8eeaf2cb2d832d17b2d00257a46ca24001
paragon0107 <smg0725@naver.com>
2024-07-17 02:33:57 +0900

    [FEAT] 메서드 분리 및 회원가입 관련 메서드 추가

--- src/main/java/com/tiki/server/auth/jwt/JwtGenerator.java
+++ src/main/java/com/tiki/server/auth/jwt/JwtGenerator.java
@@ -1,6 +1,6 @@
 package com.tiki.server.auth.jwt;
 
-import com.tiki.server.auth.dto.response.LoginResponse;
+import com.tiki.server.auth.dto.response.UserAllTokenGetResponse;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -22,39 +22,48 @@ import java.util.Date;
 @Component
 public class JwtGenerator {
 
-	@Value("${jwt.secret}")
-	private String secretKey;
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-	@Value("${jwt.access-token-expire-time}")
-	private long ACCESS_TOKEN_EXPIRE_TIME;
+    @Value("${jwt.access-token-expire-time}")
+    private long ACCESS_TOKEN_EXPIRE_TIME;
 
-	@Value("${jwt.refresh-token-expire-time}")
-	private long REFRESH_TOKEN_EXPIRE_TIME;
-	public LoginResponse generateAllToken(Authentication authentication){
-		return LoginResponse.of(
-				generateToken(authentication,ACCESS_TOKEN_EXPIRE_TIME),
-				generateToken(authentication,REFRESH_TOKEN_EXPIRE_TIME)
-		);
-	}
+    @Value("${jwt.refresh-token-expire-time}")
+    public long REFRESH_TOKEN_EXPIRE_TIME;
 
-	public String generateToken(Authentication authentication, long expiration) {
-		return Jwts.builder()
-			.setHeaderParam(TYPE, JWT_TYPE)
-			.setClaims(generateClaims(authentication))
-			.setIssuedAt(new Date(System.currentTimeMillis()))
-			.setExpiration(new Date(System.currentTimeMillis() + expiration))
-			.signWith(getSigningKey())
-			.compact();
-	}
+    public UserAllTokenGetResponse generateAllToken(Authentication authentication) {
+        return UserAllTokenGetResponse.of(
+                generateAccessToken(authentication),
+                generateRefreshToken(authentication)
+        );
+    }
 
-	private Claims generateClaims(Authentication authentication) {
-		val claims = Jwts.claims();
-		claims.put("memberId", authentication.getPrincipal());
-		return claims;
-	}
+    public String generateToken(Authentication authentication, long expiration) {
+        return Jwts.builder()
+                .setHeaderParam(TYPE, JWT_TYPE)
+                .setClaims(generateClaims(authentication))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
 
-	private SecretKey getSigningKey() {
-		val encodedKey = getEncoder().encodeToString(secretKey.getBytes());
-		return hmacShaKeyFor(encodedKey.getBytes());
-	}
+    public String generateAccessToken(Authentication authentication) {
+        return generateToken(authentication, ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    private String generateRefreshToken(Authentication authentication) {
+        return generateToken(authentication, REFRESH_TOKEN_EXPIRE_TIME);
+    }
+
+    private Claims generateClaims(Authentication authentication) {
+        val claims = Jwts.claims();
+        claims.put("memberId", authentication.getPrincipal());
+        return claims;
+    }
+
+    private SecretKey getSigningKey() {
+        val encodedKey = getEncoder().encodeToString(secretKey.getBytes());
+        return hmacShaKeyFor(encodedKey.getBytes());
+    }
 }
