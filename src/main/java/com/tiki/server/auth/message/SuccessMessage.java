471a889bcdd7276957310686f832ab1b22162a0d
paragon0107 <smg0725@naver.com>
2024-07-17 02:36:17 +0900

    [FEAT] 엑세스 토큰 관련 메세지 추가

--- src/main/java/com/tiki/server/auth/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/auth/message/SuccessMessage.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum SuccessMessage {
 
 	SUCCESS_SIGN_UP("회원가입 성공"),
-	SUCCESS_SIGN_IN("로그인 성공");
+	SUCCESS_SIGN_IN("로그인 성공"),
+	SUCCESS_GENERATE_ACCESS_TOKEN("엑세스 토큰 재발급 성공");
 
 	private final String message;
 }
