cfd5c2e8a92f37675adc870e34f7ae616653e67b
paragon0107 <smg0725@naver.com>
2024-07-16 19:20:15 +0900

    [FEAT] BelongTeamsResponse 추가

--- src/main/java/com/tiki/server/memberteammanager/dto/response/BelongTeamsResponse.java
+++ src/main/java/com/tiki/server/memberteammanager/dto/response/BelongTeamsResponse.java
@@ -1,6 +1,23 @@
 package com.tiki.server.memberteammanager.dto.response;
 
+import com.tiki.server.memberteammanager.entity.MemberTeamManager;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Builder(access = PRIVATE)
 public record BelongTeamsResponse(
-        List<BelongTeamResponse> belongTeamResponseList
+        long id,
+        @NonNull String name,
+        String iconImageUrl
 ) {
+    public static BelongTeamsResponse from(MemberTeamManager memberTeamManager) {
+        return BelongTeamsResponse.builder()
+                .id(memberTeamManager.getTeam().getId())
+                .name(memberTeamManager.getTeam().getName())
+                .iconImageUrl(memberTeamManager.getTeam().getImageUrl())
+                .build();
+    }
 }
