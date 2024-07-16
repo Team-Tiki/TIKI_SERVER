b6c4668011e5f61de76add972454361c049e3dd3
paragon0107 <smg0725@naver.com>
2024-07-16 19:22:55 +0900

    [FEAT] 서비스단 findBelongTeams구현

--- src/main/java/com/tiki/server/memberteammanager/service/MemberTeamManagerService.java
+++ src/main/java/com/tiki/server/memberteammanager/service/MemberTeamManagerService.java
@@ -2,17 +2,22 @@ package com.tiki.server.memberteammanager.service;
 
 import com.tiki.server.memberteammanager.adapter.MemberTeamManagerFinder;
 import com.tiki.server.memberteammanager.dto.response.BelongTeamsResponse;
-import lombok.val;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberTeamManagerService {
 
-    MemberTeamManagerFinder memberTeamManagerFinder;
+    private final MemberTeamManagerFinder memberTeamManagerFinder;
+
+    public List<BelongTeamsResponse> findBelongTeams(long memberId) {
+        return memberTeamManagerFinder.findBelongTeamByMemberId(memberId).stream().map(BelongTeamsResponse::from).toList();
+    }
 
 }
