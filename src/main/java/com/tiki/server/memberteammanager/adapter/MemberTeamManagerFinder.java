9adb48b3471082d8af6c3b9a2d4f141e37028992
paragon0107 <smg0725@naver.com>
2024-07-16 19:21:11 +0900

    [FEAT] MemberTeamManager Finder단 구현

--- src/main/java/com/tiki/server/memberteammanager/adapter/MemberTeamManagerFinder.java
+++ src/main/java/com/tiki/server/memberteammanager/adapter/MemberTeamManagerFinder.java
@@ -3,12 +3,13 @@ package com.tiki.server.memberteammanager.adapter;
 import static com.tiki.server.memberteammanager.message.ErrorCode.INVALID_MEMBER_TEAM_MANAGER;
 
 import com.tiki.server.common.support.RepositoryAdapter;
-import com.tiki.server.memberteammanager.dto.response.BelongTeamsResponse;
 import com.tiki.server.memberteammanager.entity.MemberTeamManager;
 import com.tiki.server.memberteammanager.exception.MemberTeamManagerException;
 import com.tiki.server.memberteammanager.repository.MemberTeamManagerRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
@@ -19,5 +20,9 @@ public class MemberTeamManagerFinder {
     public MemberTeamManager findByMemberIdAndTeamId(long memberId, long teamId) {
         return teamManagerRepository.findByMemberIdAndTeamId(memberId, teamId)
                 .orElseThrow(() -> new MemberTeamManagerException(INVALID_MEMBER_TEAM_MANAGER));
+    }
+
+    public List<MemberTeamManager> findBelongTeamByMemberId(long memberId) {
+        return teamManagerRepository.findAllBelongTeamByMemberId(memberId).stream().toList();
     }
 }
