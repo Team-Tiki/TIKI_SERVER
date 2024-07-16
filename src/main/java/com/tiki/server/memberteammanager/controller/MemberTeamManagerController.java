01363afa3a0cd8bb5cb6ee21692597f5c82ad969
paragon0107 <smg0725@naver.com>
2024-07-16 19:51:40 +0900

    [CHORE] 성공 메세지 수정

--- src/main/java/com/tiki/server/memberteammanager/controller/MemberTeamManagerController.java
+++ src/main/java/com/tiki/server/memberteammanager/controller/MemberTeamManagerController.java
@@ -1,7 +1,6 @@
 package com.tiki.server.memberteammanager.controller;
 
 import com.tiki.server.common.dto.SuccessResponse;
-import com.tiki.server.common.support.UriGenerator;
 import com.tiki.server.memberteammanager.dto.response.BelongTeamsResponse;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +16,7 @@ import java.security.Principal;
 import java.util.List;
 
 import static com.tiki.server.common.dto.SuccessResponse.success;
-import static com.tiki.server.team.message.SuccessMessage.SUCCESS_CREATE_TEAM;
+import static com.tiki.server.team.message.SuccessMessage.SUCCESS_GET_JOINED_TEAM;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,15 +25,12 @@ public class MemberTeamManagerController {
 
     private final MemberTeamManagerService memberTeamManagerService;
 
-
     @GetMapping("/joined")
     public ResponseEntity<SuccessResponse<List<BelongTeamsResponse>>> showBelongTeam(
             Principal principal
     ) {
         val memberId = Long.parseLong(principal.getName());
         val response = memberTeamManagerService.findBelongTeams(memberId);
-        return ResponseEntity.created(
-                UriGenerator.getUri("/api/v1/teams/" + response)
-        ).body(success(SUCCESS_CREATE_TEAM.getMessage(), response));
+        return ResponseEntity.ok().body(success(SUCCESS_GET_JOINED_TEAM.getMessage(), response));
     }
 }
