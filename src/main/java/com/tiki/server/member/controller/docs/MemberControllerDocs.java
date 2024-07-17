69b136ef5b3906b951d9a1d204225ed48b99e972
paragon0107 <smg0725@naver.com>
2024-07-17 19:01:10 +0900

    [FEAT] 비밀번호 변경 스웨거 추가

--- src/main/java/com/tiki/server/member/controller/docs/MemberControllerDocs.java
+++ src/main/java/com/tiki/server/member/controller/docs/MemberControllerDocs.java
@@ -3,6 +3,7 @@ package com.tiki.server.member.controller.docs;
 import com.tiki.server.common.dto.BaseResponse;
 import com.tiki.server.common.dto.ErrorResponse;
 import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.member.dto.request.ChangingPasswordRequest;
 import com.tiki.server.member.dto.request.MemberProfileCreateRequest;
 import com.tiki.server.member.dto.response.BelongTeamsGetResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -72,5 +73,34 @@ public interface MemberControllerDocs {
     )
     ResponseEntity<SuccessResponse<BelongTeamsGetResponse>> getBelongTeam(
             @Parameter(hidden = true) Principal principal
+    );
+
+    @Operation(
+            summary = "비밀번호 변경",
+            description = "비밀번호를 변경합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공",
+                            content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "비밀번호가 일치하지 않습니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "유효하지 않은 회원",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+    )
+    ResponseEntity<BaseResponse> changePassword(
+            @RequestBody ChangingPasswordRequest changingPasswordRequest
     );
 }
