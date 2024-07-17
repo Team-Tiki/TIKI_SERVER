80246ed34cd2a10c376c8b8bcb2c9f6927bd3716
paragon0107 <smg0725@naver.com>
2024-07-17 16:58:55 +0900

    [CHORE]메서드 예외 처리 추가

--- src/main/java/com/tiki/server/auth/token/adapter/TokenFinder.java
+++ src/main/java/com/tiki/server/auth/token/adapter/TokenFinder.java
@@ -1,5 +1,6 @@
 package com.tiki.server.auth.token.adapter;
 
+import com.tiki.server.auth.exception.AuthException;
 import com.tiki.server.auth.token.entity.Token;
 import com.tiki.server.auth.token.repository.TokenRepository;
 import com.tiki.server.common.support.RepositoryAdapter;
@@ -7,13 +8,15 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
+import static com.tiki.server.auth.message.ErrorCode.UNAUTHORIZED_USER;
+
 @RepositoryAdapter
 @RequiredArgsConstructor
 public class TokenFinder {
 
     private final TokenRepository tokenRepository;
 
-    public Optional<Token> findById(long id) {
-        return tokenRepository.findById(id);
+    public Token findById(long id) {
+        return tokenRepository.findById(id).orElseThrow(() -> new AuthException(UNAUTHORIZED_USER));
     }
 }
