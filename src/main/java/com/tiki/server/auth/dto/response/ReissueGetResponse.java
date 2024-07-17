dc9e4b8cc0177801bcdc61e50d8810c3f430b4b6
paragon0107 <smg0725@naver.com>
2024-07-17 16:57:57 +0900

    [CHORE] UserTokenGetResponse DTO 명 변경

--- src/main/java/com/tiki/server/auth/dto/response/ReissueGetResponse.java
+++ src/main/java/com/tiki/server/auth/dto/response/ReissueGetResponse.java
@@ -6,10 +6,11 @@ import lombok.NonNull;
 import static lombok.AccessLevel.PRIVATE;
 
 @Builder(access = PRIVATE)
-public record UserTokenGetResponse(
+public record ReissueGetResponse(
         @NonNull String accessToken
 ) {
-    public static UserTokenGetResponse from(String accessToken) {
-        return UserTokenGetResponse.builder().accessToken(accessToken).build();
+    
+    public static ReissueGetResponse from(String accessToken) {
+        return ReissueGetResponse.builder().accessToken(accessToken).build();
     }
 }
