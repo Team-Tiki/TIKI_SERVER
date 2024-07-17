0ee19e4a486411f244c92e12573057108ef8c1a0
paragon0107 <smg0725@naver.com>
2024-07-17 16:59:14 +0900

    [CHORE] 클래스명 변경

--- src/main/java/com/tiki/server/auth/token/constants/TokenConstant.java
+++ src/main/java/com/tiki/server/auth/token/constants/TokenConstant.java
@@ -2,8 +2,7 @@ package com.tiki.server.auth.token.constants;
 
 import org.springframework.beans.factory.annotation.Value;
 
-
-public class TokenConstants {
+public class TokenConstant {
 
     @Value("${jwt.refresh-token-expire-time}")
     public static int REFRESH_TOKEN_EXPIRED_TIME;
