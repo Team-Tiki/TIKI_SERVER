a995c1760c2f2adf699fb13ee61fa041c1843b38
smg0725 <smg0725@naver.com>
2024-07-29 14:56:47 +0900

    [REFACTOR] Member 존재 여부에 대한 메서드 위치 변경

--- src/main/java/com/tiki/server/member/adapter/MemberFinder.java
+++ src/main/java/com/tiki/server/member/adapter/MemberFinder.java
@@ -1,5 +1,6 @@
 package com.tiki.server.member.adapter;
 
+import static com.tiki.server.member.message.ErrorCode.CONFLICT_MEMBER;
 import static com.tiki.server.member.message.ErrorCode.INVALID_MEMBER;
 
 import com.tiki.server.common.support.RepositoryAdapter;
@@ -23,6 +24,16 @@ public class MemberFinder {
 
     public Member findById(long memberId) {
         return memberRepository.findById(memberId)
-            .orElseThrow(() -> new MemberException(INVALID_MEMBER));
+                .orElseThrow(() -> new MemberException(INVALID_MEMBER));
+    }
+
+    public Member checkEmpty(String email) {
+        return findByEmail(email).orElseThrow(() -> new MemberException(INVALID_MEMBER));
+    }
+
+    public void checkPresent(String email) {
+        findByEmail(email).ifPresent((member) -> {
+            throw new MemberException(CONFLICT_MEMBER);
+        });
     }
 }
