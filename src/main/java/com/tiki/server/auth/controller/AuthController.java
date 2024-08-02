e241e456fa787f0503e415440a5f5386d7d6c2fe
smg0725 <smg0725@naver.com>
2024-08-02 23:40:20 +0900

    [CHORE] refreshToken 쿠키 설정

--- src/main/java/com/tiki/server/auth/controller/AuthController.java
+++ src/main/java/com/tiki/server/auth/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.tiki.server.auth.controller;
 
 import com.tiki.server.auth.dto.request.LoginRequest;
-import com.tiki.server.auth.dto.response.SignInGetResponse;
 import com.tiki.server.auth.dto.response.ReissueGetResponse;
 import com.tiki.server.common.dto.SuccessResponse;
 import com.tiki.server.common.support.UriGenerator;
+import com.tiki.server.member.dto.response.AccessTokenGetResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,16 +24,28 @@ import static com.tiki.server.auth.message.SuccessMessage.*;
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
+    @Value("${JWT.EXPIRE_REFRESH}")
+    private int COOKIE_MAX_AGE;
+    private final static String REFRESH_TOKEN = "refreshToken";
     private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public ResponseEntity<SuccessResponse<SignInGetResponse>> signIn(
+    public ResponseEntity<SuccessResponse<AccessTokenGetResponse>> signIn(
             HttpServletResponse httpServletResponse,
             @RequestBody LoginRequest request
     ) {
         val response = authService.login(request, httpServletResponse);
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, response.refreshToken())
+                .maxAge(COOKIE_MAX_AGE)
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+        httpServletResponse.setHeader("Set-Cookie", cookie.toString());
         return ResponseEntity.created(UriGenerator.getUri("/"))
-                .body(SuccessResponse.success(SUCCESS_SIGN_IN.getMessage(), response));
+                .body(SuccessResponse.success(SUCCESS_SIGN_IN.getMessage(),
+                        AccessTokenGetResponse.of(response.accessToken())));
     }
 
     @GetMapping("/reissue")
