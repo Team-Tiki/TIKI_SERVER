bdeef946e2cb96b30249bcba4a29be34905821e6
Chan531 <chan657312@gmail.com>
2024-08-12 16:09:05 +0900

    [CHORE] 컨벤션 적용

--- src/main/java/com/tiki/server/document/message/ErrorCode.java
+++ src/main/java/com/tiki/server/document/message/ErrorCode.java
@@ -16,11 +16,11 @@ public enum ErrorCode {
 	/* 400 BAD_REQUEST : 잘못된 요청 */
 	INVALID_TYPE(BAD_REQUEST, "유효한 타입이 아닙니다."),
 
-	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
-	INVALID_DOCUMENT(NOT_FOUND, "유효하지 않은 문서입니다."),
-
 	/* 403 FORBIDDEN : 권한 없음 */
-	INVALID_AUTHORIZATION(FORBIDDEN, "문서에 대한 권한이 없습니다.");
+	INVALID_AUTHORIZATION(FORBIDDEN, "문서에 대한 권한이 없습니다."),
+
+	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
+	INVALID_DOCUMENT(NOT_FOUND, "유효하지 않은 문서입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
--- src/main/java/com/tiki/server/mail/message/ErrorCode.java
+++ src/main/java/com/tiki/server/mail/message/ErrorCode.java
@@ -10,13 +10,13 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    /* 403 BAD REQUEST: 인증 거부 */
+    /* 403 FORBIDDEN : 권한 없음 */
     INVALID_MATCHED(FORBIDDEN, "인증 정보가 일치하지 않습니다."),
 
-    /* 404 NOT FOUND: 요청 리소스를 찾을 수 없음 */
+    /* 404 NOT_FOUND : 자원을 찾을 수 없음 */
     INVALID_REQUEST(NOT_FOUND, "인증 정보가 존재하지 않습니다."),
 
-    /* 500 INTERNAL_SERVER_ERROR 서버 내부 오류 발생 */
+    /* 500 INTERNAL_SERVER_ERROR : 서버 내부 오류 발생 */
     MESSAGE_HELPER_ERROR(INTERNAL_SERVER_ERROR,"메세지를 설정할 수 없습니다.");
 
     private final HttpStatus httpStatus;
