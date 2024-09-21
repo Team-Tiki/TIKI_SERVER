b81b52d7afc096e48aadfb34a048d8e7ba912328
paragon0107 <smg0725@naver.com>
2024-09-21 18:05:37 +0900

    [CHORE] 이메일 검증 로직 수정

--- src/main/java/com/tiki/server/common/entity/Email.java
+++ src/main/java/com/tiki/server/common/entity/Email.java
@@ -13,6 +13,7 @@ import static com.tiki.server.member.message.ErrorCode.INVALID_EMAIL;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Email {
+
     private String email;
     public static Email from(String email){
         checkMailFormat(email);
@@ -20,7 +21,7 @@ public class Email {
     }
 
     private static void checkMailFormat(String email) {
-        if (!(email.endsWith(MAIL_FORMAT_EDU) || email.endsWith(MAIL_FORMAT_AC_KR))) {
+        if (!email.contains("@") || !(email.endsWith(MAIL_FORMAT_EDU) || email.endsWith(MAIL_FORMAT_AC_KR))) {
             throw new MemberException(INVALID_EMAIL);
         }
     }
