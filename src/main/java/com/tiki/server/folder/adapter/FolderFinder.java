dae655aba3dd0b8a57f530926955034f5fb1dfc1
Chan531 <chan657312@gmail.com>
2024-10-24 19:59:11 +0900

    [FEAT] 폴더 찾기

--- src/main/java/com/tiki/server/folder/adapter/FolderFinder.java
+++ src/main/java/com/tiki/server/folder/adapter/FolderFinder.java
@@ -1,6 +1,10 @@
 package com.tiki.server.folder.adapter;
 
+import static com.tiki.server.folder.message.ErrorCode.INVALID_FOLDER;
+
 import com.tiki.server.common.support.RepositoryAdapter;
+import com.tiki.server.folder.entity.Folder;
+import com.tiki.server.folder.exception.FolderException;
 import com.tiki.server.folder.repository.FolderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class FolderFinder {
 
 	private final FolderRepository folderRepository;
+
+	public Folder findById(long id) {
+		return folderRepository.findById(id)
+			.orElseThrow(() -> new FolderException(INVALID_FOLDER));
+	}
 }
