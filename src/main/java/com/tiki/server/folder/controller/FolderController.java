004f979948ed392e28dbd9ff7185f58b9ca172d3
Chan531 <chan657312@gmail.com>
2024-10-24 20:03:11 +0900

    [FEAT] 폴더 생성 api

--- src/main/java/com/tiki/server/folder/controller/FolderController.java
+++ src/main/java/com/tiki/server/folder/controller/FolderController.java
@@ -1,8 +1,21 @@
 package com.tiki.server.folder.controller;
 
+import static com.tiki.server.common.dto.SuccessResponse.*;
+import static com.tiki.server.folder.message.SuccessMessage.SUCCESS_CREATE_FOLDER;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.common.support.UriGenerator;
+import com.tiki.server.folder.dto.request.FolderCreateRequest;
+import com.tiki.server.folder.dto.response.FolderCreateResponse;
 import com.tiki.server.folder.service.FolderService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +27,15 @@ public class FolderController {
 
 	private final FolderService folderService;
 
-	/*
-	폴더 생성하기
-	 */
+	@PostMapping()
+	public ResponseEntity<SuccessResponse<FolderCreateResponse>> createFolder(
+		Principal principal,
+		@RequestHeader("team-id") long teamId,
+		@RequestBody FolderCreateRequest request
+	) {
+		long memberId = Long.parseLong(principal.getName());
+		FolderCreateResponse response = folderService.create(memberId, teamId, request);
+		return ResponseEntity.created(UriGenerator.getUri("api/v2/folders/" + response.folderId()))
+			.body(success(SUCCESS_CREATE_FOLDER.getMessage(), response));
+	}
 }
