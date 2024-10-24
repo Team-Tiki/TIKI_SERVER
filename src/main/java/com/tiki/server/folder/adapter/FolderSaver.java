dcfb33fa7faf5ce599ccaf5a29aa107f445ed167
Chan531 <chan657312@gmail.com>
2024-10-24 19:59:49 +0900

    [FEAT] 폴더 생성

--- src/main/java/com/tiki/server/folder/adapter/FolderSaver.java
+++ src/main/java/com/tiki/server/folder/adapter/FolderSaver.java
@@ -1,6 +1,7 @@
 package com.tiki.server.folder.adapter;
 
 import com.tiki.server.common.support.RepositoryAdapter;
+import com.tiki.server.folder.entity.Folder;
 import com.tiki.server.folder.repository.FolderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,8 @@ import lombok.RequiredArgsConstructor;
 public class FolderSaver {
 
 	private final FolderRepository folderRepository;
+
+	public Folder save(Folder folder) {
+		return folderRepository.save(folder);
+	}
 }
