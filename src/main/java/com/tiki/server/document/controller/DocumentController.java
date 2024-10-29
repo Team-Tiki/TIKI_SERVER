dd3f58eebc3b560fd09e826baf49c739d36a0188
Chan531 <chan657312@gmail.com>
2024-10-29 17:25:34 +0900

    [REFACTOR] RequestBody 누락 추가

--- src/main/java/com/tiki/server/document/controller/DocumentController.java
+++ src/main/java/com/tiki/server/document/controller/DocumentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,7 +61,7 @@ public class DocumentController implements DocumentControllerDocs {
 	public ResponseEntity<SuccessResponse<DocumentCreateResponse>> createDocument(
 		Principal principal,
 		@RequestHeader("team-id") long teamId,
-		DocumentCreateRequest request
+		@RequestBody DocumentCreateRequest request
 	) {
 		long memberId = Long.parseLong(principal.getName());
 		DocumentCreateResponse response = documentService.createDocument(memberId, teamId, request);
