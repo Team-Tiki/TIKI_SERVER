573c6c8407dbc1ae2aca1e18c0c89caefec684da
Chan531 <chan657312@gmail.com>
2024-10-29 16:27:28 +0900

    [FEAT] 파일 생성 Request

---
+++ src/main/java/com/tiki/server/document/dto/request/DocumentCreateRequest.java
@@ -0,0 +1,11 @@
+package com.tiki.server.document.dto.request;
+
+import lombok.NonNull;
+
+public record DocumentCreateRequest(
+	@NonNull String fileName,
+	@NonNull String fileUrl,
+	double capacity,
+	Long folderId
+) {
+}
