bb1710557229fee1ce69d13acdfa4ed65826f0e1
Chan531 <chan657312@gmail.com>
2024-10-29 16:54:08 +0900

    [FEAT] 파일 생성 성공 메세지 추가

--- src/main/java/com/tiki/server/document/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/document/message/SuccessMessage.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessMessage {
 
+	SUCCESS_CREATE_DOCUMENT("파일 생성 성공"),
 	SUCCESS_GET_DOCUMENTS("전체 문서 조회 성공");
 
 	private final String message;
