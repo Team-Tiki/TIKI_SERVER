87fef43a48de2e423478ddcb2e9fdfcc0c3ffe15
paragon0107 <smg0725@naver.com>
2024-11-04 02:45:15 +0900

    [FEAT] NoteFree 엔티티 추가

---
+++ src/main/java/com/tiki/server/note/entity/NoteFree.java
@@ -0,0 +1,69 @@
+package com.tiki.server.note.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Builder(access = PRIVATE)
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
+public class NoteFree {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "note_id")
+    private Long id;
+
+    private String title;
+
+    private boolean complete;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String contents;
+
+    private long teamId;
+
+    private NoteFree(
+            final String title,
+            final boolean complete,
+            final LocalDate startDate,
+            final LocalDate endDate,
+            final String contents,
+            final long teamId) {
+        this.title = title;
+        this.complete = complete;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.contents = contents;
+        this.teamId = teamId;
+    }
+
+    public static NoteFree of(
+            final String title,
+            final boolean complete,
+            final LocalDate startDate,
+            final LocalDate endDate,
+            final String contents,
+            final long teamId) {
+        return NoteFree.builder()
+                .title(title)
+                .complete(complete)
+                .startDate(startDate)
+                .endDate(endDate)
+                .contents(contents)
+                .teamId(teamId)
+                .build();
+    }
+}
