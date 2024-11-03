eab7fb40453d22acb0de321ac0b612cb19f63615
paragon0107 <smg0725@naver.com>
2024-11-04 02:48:21 +0900

    [FEAT] SuccessMessage 추가

---
+++ src/main/java/com/tiki/server/note/message/SuccessMessage.java
@@ -0,0 +1,14 @@
+package com.tiki.server.note.message;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessMessage {
+
+    CREATE_NOTE_FREE("노트 생성 성공")
+    ;
+
+    private final String message;
+}
