b9166f35249698441084fdf41f89d8a7a162e11a
paragon0107 <smg0725@naver.com>
2024-11-04 18:44:40 +0900

    [CHORE] 기존 ResponseDTO 삭제

--- src/main/java/com/tiki/server/note/dto/response/NoteCreateResponse.java
+++
@@ -1,9 +0,0 @@
-package com.tiki.server.note.dto.response;
-
-public record NoteCreateResponse(
-        long noteId
-) {
-    public static NoteCreateResponse from(final long noteId){
-        return new NoteCreateResponse(noteId);
-    }
-}
