65b7e624f7b4d1e60951ac0e28143c3c58ce6956
paragon0107 <smg0725@naver.com>
2024-11-04 18:43:32 +0900

    [CHORE] 서비스단 DTO 추가, 역의존성 제거

--- src/main/java/com/tiki/server/note/service/NoteService.java
+++ src/main/java/com/tiki/server/note/service/NoteService.java
@@ -3,11 +3,11 @@ package com.tiki.server.note.service;
 import com.tiki.server.memberteammanager.adapter.MemberTeamManagerFinder;
 import com.tiki.server.note.adapter.NoteFreeSaver;
 import com.tiki.server.note.adapter.NoteTemplateSaver;
-import com.tiki.server.note.dto.request.NoteFreeCreateRequest;
-import com.tiki.server.note.dto.request.NoteTemplateCreateRequest;
-import com.tiki.server.note.dto.response.NoteCreateResponse;
 import com.tiki.server.note.entity.NoteFree;
 import com.tiki.server.note.entity.NoteTemplate;
+import com.tiki.server.note.service.dto.request.NoteFreeCreateDTO;
+import com.tiki.server.note.service.dto.request.NoteTemplateCreateDTO;
+import com.tiki.server.note.service.dto.response.NoteCreateResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,25 +19,25 @@ public class NoteService {
     private final NoteFreeSaver noteFreeSaver;
     private final NoteTemplateSaver noteTemplateSaver;
 
-    public NoteCreateResponse createNoteFree(final long memberId, final NoteFreeCreateRequest request) {
-        memberTeamManagerFinder.findByMemberIdAndTeamId(memberId, request.teamId());
+    public NoteCreateResponseDTO createNoteFree(final NoteFreeCreateDTO request) {
+        memberTeamManagerFinder.findByMemberIdAndTeamId(request.memberId(), request.teamId());
         NoteFree noteFree = noteFreeSaver.createNoteFree(
                 NoteFree.of(
-                        request.title(),
+                        request.titleVo(),
                         request.complete(),
                         request.startDate(),
                         request.endDate(),
                         request.contents(),
                         request.teamId()
                 ));
-        return NoteCreateResponse.from(noteFree.getId());
+        return NoteCreateResponseDTO.from(noteFree.getId());
     }
 
-    public NoteCreateResponse createNoteTemplate(long memberId, NoteTemplateCreateRequest request) {
-        memberTeamManagerFinder.findByMemberIdAndTeamId(memberId, request.teamId());
+    public NoteCreateResponseDTO createNoteTemplate(final NoteTemplateCreateDTO request) {
+        memberTeamManagerFinder.findByMemberIdAndTeamId(request.memberId(), request.teamId());
         NoteTemplate noteTemplate = noteTemplateSaver.createNoteTemplate(
                 NoteTemplate.of(
-                        request.title(),
+                        request.titleVo(),
                         request.complete(),
                         request.startDate(),
                         request.endDate(),
@@ -47,6 +47,6 @@ public class NoteService {
                         request.answerHowToFix(),
                         request.teamId()
                 ));
-        return NoteCreateResponse.from(noteTemplate.getId());
+        return NoteCreateResponseDTO.from(noteTemplate.getId());
     }
 }
