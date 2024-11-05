0f6459ae416c20f6a455cf417b9d0897203671d5
paragon0107 <smg0725@naver.com>
2024-11-05 21:53:16 +0900

    [CHORE] 서비스단 Free, Template DTO 재분리

--- src/main/java/com/tiki/server/note/service/dto/request/NoteFreeCreateDTO.java
+++ src/main/java/com/tiki/server/note/service/dto/request/NoteFreeCreateDTO.java
@@ -1,12 +1,12 @@
 package com.tiki.server.note.service.dto.request;
 
-import com.tiki.server.note.controller.dto.request.NoteCreateRequest;
+import com.tiki.server.note.controller.dto.request.NoteFreeCreateRequest;
 import lombok.NonNull;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record NoteCreateDTO(
+public record NoteFreeCreateDTO(
         @NonNull String title,
         boolean complete,
         @NonNull LocalDate startDate,
@@ -17,18 +17,18 @@ public record NoteCreateDTO(
         List<Long> documentIds,
         long memberId
 ) {
-    public static NoteCreateDTO of(
-            final NoteCreateRequest request,
+    public static NoteFreeCreateDTO of(
+            final NoteFreeCreateRequest request,
             final long memberId
     ) {
-        return new NoteCreateDTO(
+        return new NoteFreeCreateDTO(
                 request.title(),
                 request.complete(),
                 request.startDate(),
                 request.endDate(),
                 request.contents(),
                 request.teamId(),
-                request.timBlockIds(),
+                request.timeBlockIds(),
                 request.documentIds(),
                 memberId
         );
