d35de2cb7fa59858d4ece5333e12f19a4afa9672
paragon0107 <smg0725@naver.com>
2024-11-08 13:26:54 +0900

    [FEAT] 노트 Deleter 구현

---
+++ src/main/java/com/tiki/server/note/adapter/NoteDeleter.java
@@ -0,0 +1,18 @@
+package com.tiki.server.note.adapter;
+
+import com.tiki.server.common.support.RepositoryAdapter;
+import com.tiki.server.note.repository.NoteRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RepositoryAdapter
+@RequiredArgsConstructor
+public class NoteDeleter {
+
+    private final NoteRepository noteRepository;
+
+    public void deleteNoteByIds(final List<Long> noteIds){
+        noteIds.forEach(noteRepository::deleteById);
+    }
+}
