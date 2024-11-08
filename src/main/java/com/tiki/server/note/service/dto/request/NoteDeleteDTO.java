05c82783eb9a8b502ef3ad4df0700e2f1dadae6e
paragon0107 <smg0725@naver.com>
2024-11-08 13:25:12 +0900

    [CHORE] 노트 삭제 서비스레이어 DTO 구현

---
+++ src/main/java/com/tiki/server/note/service/dto/request/NoteDeleteDTO.java
@@ -0,0 +1,13 @@
+package com.tiki.server.note.service.dto.request;
+
+import java.util.List;
+
+public record NoteDeleteDTO(
+        List<Long> notesIds,
+        long teamId,
+        long memberId
+) {
+    public static NoteDeleteDTO of(List<Long> notesIds,long teamId,long memberId){
+        return new NoteDeleteDTO(notesIds, teamId, memberId);
+    }
+}
