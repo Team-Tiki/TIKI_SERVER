383cabf021de56051564d9e9bca1e302ba3d0c53
paragon0107 <smg0725@naver.com>
2024-11-08 13:28:32 +0900

    [CHORE] 잘못 만든 Repository 삭제

--- src/main/java/com/tiki/server/notedocumentmanager/repository/NoteDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tiki.server.notedocumentmanager.repository;
-
-import com.tiki.server.notedocumentmanager.entity.NoteDocumentManager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteDocumentRepository extends JpaRepository<NoteDocumentManager, Long> {
-}
