3fd6487a023f0dbf50cbd44b51edb0f4b6c6e462
paragon0107 <smg0725@naver.com>
2024-11-08 13:23:30 +0900

    [CHORE] NoteController 삭제, 조회부분 구현

--- src/main/java/com/tiki/server/note/controller/NoteController.java
+++ src/main/java/com/tiki/server/note/controller/NoteController.java
@@ -5,19 +5,21 @@ import com.tiki.server.common.support.UriGenerator;
 import com.tiki.server.note.controller.dto.request.NoteFreeCreateRequest;
 import com.tiki.server.note.controller.dto.request.NoteTemplateCreateRequest;
 import com.tiki.server.note.service.NoteService;
+import com.tiki.server.note.service.dto.request.NoteDeleteDTO;
 import com.tiki.server.note.service.dto.request.NoteFreeCreateDTO;
 import com.tiki.server.note.service.dto.request.NoteTemplateCreateDTO;
 import com.tiki.server.note.service.dto.response.NoteCreateResponseDTO;
-import com.tiki.server.note.service.dto.response.NoteGetResponseDTO;
+import com.tiki.server.note.service.dto.response.NoteGetDetailResponseDTO;
+import com.tiki.server.note.service.dto.response.NoteGetListResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 import static com.tiki.server.common.dto.SuccessResponse.success;
-import static com.tiki.server.note.message.SuccessMessage.CREATE_NOTE_FREE;
-import static com.tiki.server.note.message.SuccessMessage.GET_NOTE;
+import static com.tiki.server.note.message.SuccessMessage.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,7 +37,7 @@ public class NoteController {
         NoteCreateResponseDTO response = noteService.createNoteFree(NoteFreeCreateDTO.of(request, memberId));
         return ResponseEntity.created(
                 UriGenerator.getUri("/api/v1/notes" + response.noteId())
-        ).body(success(CREATE_NOTE_FREE.getMessage(), response));
+        ).body(success(CREATE_NOTE.getMessage(), response));
     }
 
     @PostMapping("/template")
@@ -47,16 +49,38 @@ public class NoteController {
         NoteCreateResponseDTO response = noteService.createNoteTemplate(NoteTemplateCreateDTO.of(request, memberId));
         return ResponseEntity.created(
                 UriGenerator.getUri("/api/v1/notes" + response.noteId())
-        ).body(success(CREATE_NOTE_FREE.getMessage(), response));
+        ).body(success(CREATE_NOTE.getMessage(), response));
     }
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<SuccessResponse<NoteGetResponseDTO>> getNote(
+    public ResponseEntity<SuccessResponse<NoteGetListResponseDTO>> getNote(
             final Principal principal,
             @PathVariable long teamId
     ){
         long memberId = Long.parseLong(principal.getName());
-        NoteGetResponseDTO response = noteService.getNote(teamId,memberId);
+        NoteGetListResponseDTO response = noteService.getNote(teamId,memberId);
         return ResponseEntity.ok().body(success(GET_NOTE.getMessage(), response));
+    }
+
+    @GetMapping("/{teamId}/{noteId}")
+    public ResponseEntity<SuccessResponse<NoteGetDetailResponseDTO>> getNoteDetail(
+            final Principal principal,
+            @PathVariable final long teamId,
+            @PathVariable final long noteId
+    ){
+        long memberId = Long.parseLong(principal.getName());
+        NoteGetDetailResponseDTO response = noteService.getNoteDetail(teamId,memberId,noteId);
+        return ResponseEntity.ok().body(success(GET_NOTE_DETAIL.getMessage(), response));
+    }
+
+    @DeleteMapping("/{teamId}")
+    public ResponseEntity<SuccessResponse<Void>> deleteNotes(
+            final Principal principal,
+            @PathVariable long teamId,
+            @RequestParam List<Long> noteIds
+    ){
+        long memberId = Long.parseLong(principal.getName());
+        noteService.deleteNotes(NoteDeleteDTO.of(noteIds,teamId,memberId));
+        return ResponseEntity.ok().body(success(DELETE_NOTE.getMessage(),null));
     }
 }
