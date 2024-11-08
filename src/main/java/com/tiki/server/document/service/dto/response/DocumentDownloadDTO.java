ed196ec9be831b739ae28264f4c195a759c9b2d4
paragon0107 <smg0725@naver.com>
2024-11-08 13:34:11 +0900

    [FEAT] 노트 조회 과련 다른 도메인 DTO 구현

---
+++ src/main/java/com/tiki/server/document/service/dto/response/DocumentDownloadDTO.java
@@ -0,0 +1,18 @@
+package com.tiki.server.document.service.dto.response;
+
+import com.tiki.server.document.entity.Document;
+
+public record DocumentDownloadDTO(
+        long id,
+        String fileName,
+        String fileUrl,
+        double capacity
+) {
+    public static DocumentDownloadDTO of(final Document document) {
+        return new DocumentDownloadDTO(
+                document.getId(),
+                document.getFileName(),
+                document.getFileUrl(),
+                document.getCapacity());
+    }
+}
---
+++ src/main/java/com/tiki/server/timeblock/service/dto/response/TimeBlockNameDTO.java
@@ -0,0 +1,7 @@
+package com.tiki.server.timeblock.service.dto.response;
+
+public record TimeBlockNameDTO(
+        long id,
+        String name
+) {
+}
