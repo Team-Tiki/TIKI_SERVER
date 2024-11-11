bb1e8a23c95809093cdefaad574fb669577c5f43
paragon0107 <smg0725@naver.com>
2024-11-11 16:53:57 +0900

    [FEAT] ContentDecoder 추가

--- src/main/java/com/tiki/server/common/util/ContentDecoder.java
+++ src/main/java/com/tiki/server/common/util/ContentDecoder.java
@@ -1,11 +1,21 @@
 package com.tiki.server.common.util;
 
 import java.util.Base64;
+import java.util.List;
 
 public class ContentDecoder {
 
-    public static String encodeNoteFree(final String contents) {
-        byte[] decodedBytes = Base64.getDecoder().decode(contents);
+    public static List<String> decodeNoteTemplate(final String encodedData) {
+        String[] parts = encodedData.split("\\|");
+        String decodedActivity = new String(Base64.getDecoder().decode(parts[0]));
+        String decodedPrepare = new String(Base64.getDecoder().decode(parts[1]));
+        String decodedDisappointing = new String(Base64.getDecoder().decode(parts[2]));
+        String decodedComplement = new String(Base64.getDecoder().decode(parts[3]));
+        return List.of(decodedActivity, decodedPrepare, decodedDisappointing, decodedComplement);
+    }
+
+    public static String decodeNoteFree(final String encodeDate) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodeDate);
         return new String(decodedBytes);
     }
 }
