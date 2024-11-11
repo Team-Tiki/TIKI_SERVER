4477aef886fd8fcf90b4143f208ef96d507f16fb
paragon0107 <smg0725@naver.com>
2024-11-11 18:08:56 +0900

    [FEAT] NoteRepository 페이지네이션 메서드 추가

--- src/main/java/com/tiki/server/note/repository/NoteRepository.java
+++ src/main/java/com/tiki/server/note/repository/NoteRepository.java
@@ -1,11 +1,19 @@
 package com.tiki.server.note.repository;
 
 import com.tiki.server.note.entity.Note;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
-    List<Note> findAllByTeamId(final long teamId);
+    @Query("SELECT n FROM Note n WHERE n.updatedAt < :lastModifiedAt ORDER BY n.updatedAt DESC")
+    List<Note> findByModifiedAtBeforeOrderByModifiedAtDesc(@Param("lastModifiedAt") LocalDateTime lastModifiedAt, Pageable pageable);
+
+    @Query("SELECT n FROM Note n WHERE n.updatedAt > :lastModifiedAt ORDER BY n.updatedAt ASC")
+    List<Note> findByModifiedAtAfterOrderByModifiedAtAsc(@Param("lastModifiedAt") LocalDateTime lastModifiedAt, Pageable pageable);
 }
