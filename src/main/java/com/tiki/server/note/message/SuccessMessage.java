b50bd3895390bf4f8e0951391d7bbf09f0b8f75d
paragon0107 <smg0725@naver.com>
2024-11-11 21:55:33 +0900

    [CHORE] 삭제 성공 메세지 삭제

--- src/main/java/com/tiki/server/note/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/note/message/SuccessMessage.java
@@ -9,8 +9,7 @@ public enum SuccessMessage {
 
     GET_NOTE("노트 조회 성공"),
     GET_NOTE_DETAIL("노트 상세 조회 성공"),
-    CREATE_NOTE("노트 생성 성공"),
-    DELETE_NOTE("노트 삭제 성공");
+    CREATE_NOTE("노트 생성 성공");
 
     private final String message;
 }
