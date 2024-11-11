43a7633eb8c897e46f90f2d8fabec2400d5279fe
paragon0107 <smg0725@naver.com>
2024-11-12 01:57:32 +0900

    [FEAT] 컨트롤러 단 DTO 구현

---
+++ src/main/java/com/tiki/server/note/controller/dto/request/NoteTemplateUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.tiki.server.note.controller.dto.request;
+
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record NoteTemplateUpdateRequest(
+        @NonNull
+        String title,
+        boolean complete,
+        @NonNull LocalDate startDate,
+        @NonNull LocalDate endDate,
+        @NonNull String answerWhatActivity,
+        @NonNull String answerHowToPrepare,
+        @NonNull String answerWhatIsDisappointedThing,
+        @NonNull String answerHowToFix,
+        @NonNull List<Long> timeBlockIds,
+        @NonNull List<Long> documentIds,
+        long teamId
+) {
+}
