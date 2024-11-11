6e71c28f6c9ea4c183beee84142066ce4f376c34
paragon0107 <smg0725@naver.com>
2024-11-11 18:10:11 +0900

    [CHORE] 노트 조회 반환타입 수정

--- src/main/java/com/tiki/server/note/service/dto/response/NoteGetResponseDTO.java
+++ src/main/java/com/tiki/server/note/service/dto/response/NoteGetResponseDTO.java
@@ -3,16 +3,19 @@ package com.tiki.server.note.service.dto.response;
 import com.tiki.server.note.entity.Note;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record NoteGetResponseDTO(
+        long noteId,
         String title,
         LocalDate startDate,
         LocalDate endDate,
         String author,
-        boolean complete
+        boolean complete,
+        LocalDateTime lastUpdatedAt
 ) {
 
-    public static NoteGetResponseDTO of(final Note note,final String memberName){
-        return new NoteGetResponseDTO(note.getTitle(), note.getStartDate(),note.getEndDate(),memberName, note.isComplete());
+    public static NoteGetResponseDTO of(final Note note) {
+        return new NoteGetResponseDTO(note.getId(), note.getTitle(), note.getStartDate(), note.getEndDate(), note.getAuthor(), note.isComplete(), note.getUpdatedAt());
     }
 }
