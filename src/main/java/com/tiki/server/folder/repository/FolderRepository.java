48170f9141381c3a987c1d7f3ca4a192da786d2e
Chan531 <chan657312@gmail.com>
2024-11-13 16:40:55 +0900

    feat(FolderRepository): 폴더 찾기
    path로 폴더 찾기

--- src/main/java/com/tiki/server/folder/repository/FolderRepository.java
+++ src/main/java/com/tiki/server/folder/repository/FolderRepository.java
@@ -1,8 +1,11 @@
 package com.tiki.server.folder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tiki.server.folder.entity.Folder;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+	List<Folder> findFolderByPath(String path);
 }
