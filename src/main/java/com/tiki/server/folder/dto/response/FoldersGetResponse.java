f2af538a34a9a2b415adaf70539abec4d22c3629
Chan531 <chan657312@gmail.com>
2024-11-13 16:50:30 +0900

    feat(FoldersGetResponse): 폴더 조회 dto

--- src/main/java/com/tiki/server/folder/dto/response/FoldersGetResponse.java
+++ src/main/java/com/tiki/server/folder/dto/response/FoldersGetResponse.java
@@ -1,4 +1,33 @@
 package com.tiki.server.folder.dto.response;
 
-public record FoldersGetResponse() {
+import static lombok.AccessLevel.PRIVATE;
+
+import java.time.LocalDateTime;
+
+import com.tiki.server.folder.entity.Folder;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder(access = PRIVATE)
+public record FoldersGetResponse(
+) {
+
+	@Builder(access = PRIVATE)
+	private record FolderGetResponse(
+		long id,
+		@NonNull String name,
+		@NonNull LocalDateTime createdTime,
+		@NonNull String path
+	) {
+
+		private static FolderGetResponse from(Folder folder) {
+			return FolderGetResponse.builder()
+					.id(folder.getId())
+					.name(folder.getName())
+					.createdTime(folder.getCreatedAt())
+					.path(folder.getPath())
+					.build();
+		}
+	}
 }
