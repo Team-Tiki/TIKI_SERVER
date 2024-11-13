a6d4e31ada7b24282231f366b00e8c9a04a5f740
Chan531 <chan657312@gmail.com>
2024-11-13 16:53:04 +0900

    feat(SuccessMessage): 폴더 목록 조회 성공 메세지

--- src/main/java/com/tiki/server/folder/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/folder/message/SuccessMessage.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessMessage {
 
+	SUCCESS_GET_FOLDERS("폴더 목록 조회 성공"),
 	SUCCESS_CREATE_FOLDER("폴더 생성 성공");
 
 	private final String message;
