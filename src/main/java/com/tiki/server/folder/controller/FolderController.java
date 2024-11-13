fa36059c181daeb4c516f83250827daa49066a6b
Chan531 <chan657312@gmail.com>
2024-11-13 16:53:34 +0900

    feat(FolderController): 폴더 조회 api

--- src/main/java/com/tiki/server/folder/controller/FolderController.java
+++ src/main/java/com/tiki/server/folder/controller/FolderController.java
@@ -1,21 +1,26 @@
 package com.tiki.server.folder.controller;
 
 import static com.tiki.server.common.dto.SuccessResponse.*;
+import static com.tiki.server.folder.constant.Constant.ROOT_PATH;
 import static com.tiki.server.folder.message.SuccessMessage.SUCCESS_CREATE_FOLDER;
+import static com.tiki.server.folder.message.SuccessMessage.SUCCESS_GET_FOLDERS;
 
 import java.security.Principal;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiki.server.common.dto.SuccessResponse;
 import com.tiki.server.common.support.UriGenerator;
 import com.tiki.server.folder.dto.request.FolderCreateRequest;
 import com.tiki.server.folder.dto.response.FolderCreateResponse;
+import com.tiki.server.folder.dto.response.FoldersGetResponse;
 import com.tiki.server.folder.service.FolderService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +31,17 @@ import lombok.RequiredArgsConstructor;
 public class FolderController {
 
 	private final FolderService folderService;
+
+	@GetMapping()
+	public ResponseEntity<SuccessResponse<FoldersGetResponse>> getFolders(
+		final Principal principal,
+		@RequestHeader("team-id") long teamId,
+		@RequestParam(defaultValue = ROOT_PATH) String path
+	) {
+		long memberId = Long.parseLong(principal.getName());
+		FoldersGetResponse response = folderService.get(memberId, teamId, path);
+		return ResponseEntity.ok(success(SUCCESS_GET_FOLDERS.getMessage(), response));
+	}
 
 	@PostMapping()
 	public ResponseEntity<SuccessResponse<FolderCreateResponse>> createFolder(
