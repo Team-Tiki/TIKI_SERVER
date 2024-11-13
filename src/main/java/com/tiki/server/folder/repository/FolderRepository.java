303c8dbf86da39aaa185d523ae90963c7d2b9a19
Chan531 <chan657312@gmail.com>
2024-11-13 16:41:25 +0900

    feat(FolderRepository): 폴더 찾기
    path로 시작하는 폴더 찾기

--- src/main/java/com/tiki/server/folder/repository/FolderRepository.java
+++ src/main/java/com/tiki/server/folder/repository/FolderRepository.java
@@ -8,4 +8,5 @@ import com.tiki.server.folder.entity.Folder;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 	List<Folder> findFolderByPath(String path);
+	List<Folder> findFolderByPathStartingWith(String path);
 }
