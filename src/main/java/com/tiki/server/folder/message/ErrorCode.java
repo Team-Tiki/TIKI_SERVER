f50af724abf8f0940d3af576c1f04f42f1a83232
Chan531 <chan657312@gmail.com>
2024-11-19 16:21:34 +0900

    feat(ErrorCode): 중복된 폴더 이름 관련 에러 코드 추가

--- src/main/java/com/tiki/server/folder/message/ErrorCode.java
+++ src/main/java/com/tiki/server/folder/message/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.tiki.server.folder.message;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
-	INVALID_FOLDER(NOT_FOUND, "유효하지 않은 폴더입니다.");
+	INVALID_FOLDER(NOT_FOUND, "유효하지 않은 폴더입니다."),
+
+	/* 409 CONFLICT : 중복된 자원 */
+	FOLDER_NAME_DUPLICATE(CONFLICT, "중복된 폴더 이름입니다.");;
 
 	private final HttpStatus httpStatus;
 	private final String message;
