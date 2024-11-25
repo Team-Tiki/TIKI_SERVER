21bbc7e2ef1a07dd1629c0b361e0d2545269dde8
Chan531 <chan657312@gmail.com>
2024-11-25 19:44:58 +0900

    feat(DeletedDocumentAdapter): id와 팀 id로 파일 조회

--- src/main/java/com/tiki/server/document/adapter/DeletedDocumentAdapter.java
+++ src/main/java/com/tiki/server/document/adapter/DeletedDocumentAdapter.java
@@ -1,10 +1,13 @@
 package com.tiki.server.document.adapter;
 
+import static com.tiki.server.document.message.ErrorCode.INVALID_DOCUMENT;
+
 import java.util.List;
 
 import com.tiki.server.common.support.RepositoryAdapter;
 import com.tiki.server.document.entity.DeletedDocument;
 import com.tiki.server.document.entity.Document;
+import com.tiki.server.document.exception.DocumentException;
 import com.tiki.server.document.repository.DeletedDocumentRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +22,18 @@ public class DeletedDocumentAdapter {
 		documents.forEach(document -> deletedDocumentRepository.save(create(document, teamId)));
 	}
 
+	public List<DeletedDocument> get(final List<Long> deletedDocumentIds, final long teamId) {
+		return deletedDocumentIds.stream()
+				.map(id -> find(id, teamId))
+				.toList();
+	}
+
 	private DeletedDocument create(final Document document, final long teamId) {
 		return DeletedDocument.of(document.getFileName(), document.getFileUrl(), teamId, document.getCapacity());
+	}
+
+	private DeletedDocument find(final long id, final long teamId) {
+		return deletedDocumentRepository.findByIdAndTeamId(id, teamId)
+				.orElseThrow(() -> new DocumentException(INVALID_DOCUMENT));
 	}
 }
