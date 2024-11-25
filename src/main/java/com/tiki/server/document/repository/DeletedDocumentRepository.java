c07f50244348318ff32df2f11b794afc7452ed82
Chan531 <chan657312@gmail.com>
2024-11-25 19:44:20 +0900

    feat(DeletedDocumentRepository): id와 팀 id로 파일 조회

--- src/main/java/com/tiki/server/document/repository/DeletedDocumentRepository.java
+++ src/main/java/com/tiki/server/document/repository/DeletedDocumentRepository.java
@@ -1,8 +1,12 @@
 package com.tiki.server.document.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tiki.server.document.entity.DeletedDocument;
 
 public interface DeletedDocumentRepository extends JpaRepository<DeletedDocument, Long> {
+
+	Optional<DeletedDocument> findByIdAndTeamId(long id, long teamId);
 }
