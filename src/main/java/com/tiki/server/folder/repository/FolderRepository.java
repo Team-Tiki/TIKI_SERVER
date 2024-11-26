361ba79abd3afc424f44f61f0c1d88c4968fec09
Chan531 <chan657312@gmail.com>
2024-11-26 15:33:10 +0900

    feat(FolderRepository): id와 팀 id로 폴더 조회

--- src/main/java/com/tiki/server/folder/repository/FolderRepository.java
+++ src/main/java/com/tiki/server/folder/repository/FolderRepository.java
@@ -1,6 +1,7 @@
 package com.tiki.server.folder.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import com.tiki.server.folder.entity.Folder;
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 	List<Folder> findAllByTeamIdAndPathOrderByCreatedAtDesc(long teamId, String path);
 	List<Folder> findAllByPathOrderByCreatedAtDesc(String path);
+	Optional<Folder> findByIdAndTeamId(long id, long teamId);
 }
