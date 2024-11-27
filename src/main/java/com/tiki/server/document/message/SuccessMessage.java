604b1fe6a998c0392ce3e29c8ad381a4b889e649
Chan531 <chan657312@gmail.com>
2024-11-27 16:42:47 +0900

    feat(SuccessMessage): 휴지통 조회 성공 메세지 추가

--- src/main/java/com/tiki/server/document/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/document/message/SuccessMessage.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum SuccessMessage {
 
 	SUCCESS_CREATE_DOCUMENTS("파일 생성 성공"),
-	SUCCESS_GET_DOCUMENTS("전체 문서 조회 성공");
+	SUCCESS_GET_DOCUMENTS("전체 문서 조회 성공"),
+	SUCCESS_GET_TRASH("휴지통 조회 성공");
 
 	private final String message;
 }
