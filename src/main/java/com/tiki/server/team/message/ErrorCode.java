b5c27739ea43ce8511212dfbc61b05fcb2055c56
paragon0107 <smg0725@naver.com>
2024-11-29 18:33:15 +0900

    [FEAT] 권한이 어드민일시 터지는 에러메세지 추가 및 기존 메세지 수정

--- src/main/java/com/tiki/server/team/message/ErrorCode.java
+++ src/main/java/com/tiki/server/team/message/ErrorCode.java
@@ -13,7 +13,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	/* 403 FORBIDDEN : 권한 없음 */
-	INVALID_AUTHORIZATION_DELETE(FORBIDDEN, "팀 삭제에 대한 권한이 없습니다."),
+	INVALID_AUTHORIZATION_DELETE(FORBIDDEN, "권한이 없습니다."),
+	TOO_HIGH_AUTHORIZATION(FORBIDDEN, "어드민은 진행할 수 없습니다."),
 
 	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
 	INVALID_TEAM(NOT_FOUND, "유효하지 않은 단체입니다.");
