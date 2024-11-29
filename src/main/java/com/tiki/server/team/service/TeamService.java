a658f58ea920d3a113cbb7723bb0af5b9f8282b9
paragon0107 <smg0725@naver.com>
2024-11-29 17:42:51 +0900

    [FEAT] 아이콘 변경, 어드민 권한 위임기능 서비스단 추가

--- src/main/java/com/tiki/server/team/service/TeamService.java
+++ src/main/java/com/tiki/server/team/service/TeamService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import com.tiki.server.document.adapter.DocumentDeleter;
 import com.tiki.server.document.adapter.DocumentFinder;
 import com.tiki.server.document.entity.Document;
+import com.tiki.server.external.util.S3Handler;
 import com.tiki.server.memberteammanager.adapter.MemberTeamManagerDeleter;
 import com.tiki.server.memberteammanager.adapter.MemberTeamManagerFinder;
 import com.tiki.server.team.adapter.TeamDeleter;
@@ -40,68 +41,85 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class TeamService {
 
-	private final TeamSaver teamSaver;
-	private final TeamFinder teamFinder;
-	private final TeamDeleter teamDeleter;
-	private final MemberFinder memberFinder;
-	private final DocumentFinder documentFinder;
-	private final DocumentDeleter documentDeleter;
-	private final TimeBlockDeleter timeBlockDeleter;
-	private final MemberTeamManagerFinder memberTeamManagerFinder;
-	private final MemberTeamManagerDeleter memberTeamManagerDeleter;
-	private final MemberTeamManagerSaver memberTeamManagerSaver;
+    private final TeamSaver teamSaver;
+    private final TeamFinder teamFinder;
+    private final TeamDeleter teamDeleter;
+    private final MemberFinder memberFinder;
+    private final DocumentFinder documentFinder;
+    private final DocumentDeleter documentDeleter;
+    private final TimeBlockDeleter timeBlockDeleter;
+    private final MemberTeamManagerFinder memberTeamManagerFinder;
+    private final MemberTeamManagerDeleter memberTeamManagerDeleter;
+    private final MemberTeamManagerSaver memberTeamManagerSaver;
+    private final S3Handler s3Handler;
 
-	@Transactional
-	public TeamCreateResponse createTeam(long memberId, TeamCreateRequest request) {
-		Member member = memberFinder.findById(memberId);
-		Team team = teamSaver.save(createTeam(request, member.getUniv()));
-		memberTeamManagerSaver.save(createMemberTeamManager(member, team, ADMIN));
-		return TeamCreateResponse.from(team);
-	}
+    @Transactional
+    public TeamCreateResponse createTeam(long memberId, TeamCreateRequest request) {
+        Member member = memberFinder.findById(memberId);
+        Team team = teamSaver.save(createTeam(request, member.getUniv()));
+        memberTeamManagerSaver.save(createMemberTeamManager(member, team, ADMIN));
+        return TeamCreateResponse.from(team);
+    }
 
-	public TeamsGetResponse getAllTeams(long memberId) {
-		Member member = memberFinder.findById(memberId);
-		University univ = member.getUniv();
-		List<TeamVO> team = teamFinder.findAllByUniv(univ);
-		return TeamsGetResponse.from(team);
-	}
+    public TeamsGetResponse getAllTeams(long memberId) {
+        Member member = memberFinder.findById(memberId);
+        University univ = member.getUniv();
+        List<TeamVO> team = teamFinder.findAllByUniv(univ);
+        return TeamsGetResponse.from(team);
+    }
 
-	public CategoriesGetResponse getCategories() {
-		Category[] categories = Category.values();
-		return CategoriesGetResponse.from(categories);
-	}
+    public CategoriesGetResponse getCategories() {
+        Category[] categories = Category.values();
+        return CategoriesGetResponse.from(categories);
+    }
 
-	@Transactional
-	public void deleteTeam(long memberId, long teamId) {
-		checkIsAdmin(memberId, teamId);
-		List<MemberTeamManager> memberTeamManagers = memberTeamManagerFinder.findAllByTeamId(teamId);
-		memberTeamManagerDeleter.deleteAll(memberTeamManagers);
-		List<Document> documents = documentFinder.findAllByTeamId(teamId);
-		documentDeleter.deleteAll(documents);
-		timeBlockDeleter.deleteAllByTeamId(teamId);
-		teamDeleter.deleteById(teamId);
-	}
+    @Transactional
+    public void deleteTeam(long memberId, long teamId) {
+        checkIsAdmin(memberId, teamId);
+        List<MemberTeamManager> memberTeamManagers = memberTeamManagerFinder.findAllByTeamId(teamId);
+        memberTeamManagerDeleter.deleteAll(memberTeamManagers);
+        List<Document> documents = documentFinder.findAllByTeamId(teamId);
+        documentDeleter.deleteAll(documents);
+        timeBlockDeleter.deleteAllByTeamId(teamId);
+        teamDeleter.deleteById(teamId);
+    }
 
-	@Transactional
-	public void updateTeamName(final long memberId,final long teamId,final String newTeamName){
-		checkIsAdmin(memberId, teamId);
-		Team team = teamFinder.findById(teamId);
-		team.updateTeamName(newTeamName);
-	}
+    @Transactional
+    public void updateTeamName(final long memberId, final long teamId, final String newTeamName) {
+        checkIsAdmin(memberId, teamId);
+        Team team = teamFinder.findById(teamId);
+        team.setName(newTeamName);
+    }
 
-	private Team createTeam(TeamCreateRequest request, University univ) {
-		return Team.of(request, univ);
-	}
+    @Transactional
+    public void updateIconImage(final long memberId, final long teamId, final String iconImageUrl) {
+        checkIsAdmin(memberId, teamId);
+        Team team = teamFinder.findById(teamId);
+        s3Handler.deleteFile(team.getIconImageUrl());
+        team.setIconImageUrl(iconImageUrl);
+    }
 
-	private MemberTeamManager createMemberTeamManager(Member member, Team team, Position position) {
-		return MemberTeamManager.of(member, team, position);
-	}
+    @Transactional
+    public void alterAdmin(final long memberId, final long teamId, final long targetId) {
+        MemberTeamManager oldAdmin = checkIsAdmin(memberId, teamId);
+        MemberTeamManager newAdmin = memberTeamManagerFinder.findByMemberIdAndTeamIdOrElseThrow(memberId, teamId);
+        oldAdmin.setPositionToExecutive();
+        newAdmin.setPositionToAdmin();
+    }
 
-	private void checkIsAdmin(long memberId, long teamId) {
-		MemberTeamManager memberTeamManager = memberTeamManagerFinder.findByMemberIdAndTeamIdOrElseThrow(memberId, teamId);
-		if (!memberTeamManager.getPosition().equals(ADMIN)) {
-			throw new TeamException(INVALID_AUTHORIZATION_DELETE);
-		}
-		memberTeamManagerDeleter.delete(memberTeamManager);
-	}
+    private Team createTeam(TeamCreateRequest request, University univ) {
+        return Team.of(request, univ);
+    }
+
+    private MemberTeamManager createMemberTeamManager(Member member, Team team, Position position) {
+        return MemberTeamManager.of(member, team, position);
+    }
+
+    private MemberTeamManager checkIsAdmin(long memberId, long teamId) {
+        MemberTeamManager memberTeamManager = memberTeamManagerFinder.findByMemberIdAndTeamIdOrElseThrow(memberId, teamId);
+        if (!memberTeamManager.getPosition().equals(ADMIN)) {
+            throw new TeamException(INVALID_AUTHORIZATION_DELETE);
+        }
+        return memberTeamManager;
+    }
 }
