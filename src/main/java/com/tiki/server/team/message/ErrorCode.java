961a6340c17f611b315cfa0deb02825e10d59e1d
paragon0107 <smg0725@naver.com>
2024-11-29 18:41:02 +0900

    [FEAT] 권한이 어드민일시 터지는 에러메세지 추가 및 기존 메세지 수정

--- src/main/java/com/tiki/server/team/message/ErrorCode.java
+++ src/main/java/com/tiki/server/team/message/ErrorCode.java
@@ -1,20 +1,21 @@
 package com.tiki.server.team.message;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import org.springframework.http.HttpStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
+	/* 400 BAD_REQUEST : 잘못된 요청 */
+	TOO_HIGH_AUTHORIZATION(BAD_REQUEST, "어드민은 진행할 수 없습니다."),
+
 	/* 403 FORBIDDEN : 권한 없음 */
 	INVALID_AUTHORIZATION_DELETE(FORBIDDEN, "권한이 없습니다."),
-	TOO_HIGH_AUTHORIZATION(FORBIDDEN, "어드민은 진행할 수 없습니다."),
 
 	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
 	INVALID_TEAM(NOT_FOUND, "유효하지 않은 단체입니다.");
