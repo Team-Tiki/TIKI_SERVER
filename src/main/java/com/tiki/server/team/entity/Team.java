620f46398affc569169121104021e73e11fd9724
smg0725 <smg0725@naver.com>
2024-12-03 02:45:20 +0900

    [CHORE] 팀 이름 변경 정책 추가

--- src/main/java/com/tiki/server/team/entity/Team.java
+++ src/main/java/com/tiki/server/team/entity/Team.java
@@ -1,5 +1,6 @@
 package com.tiki.server.team.entity;
 
+import static com.tiki.server.team.message.ErrorCode.TOO_SHORT_PERIOD;
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PRIVATE;
@@ -9,11 +10,14 @@ import com.tiki.server.common.entity.BaseTime;
 import com.tiki.server.common.entity.University;
 import com.tiki.server.team.dto.request.TeamCreateRequest;
 
+import com.tiki.server.team.exception.TeamException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -44,18 +48,24 @@ public class Team extends BaseTime {
     private String imageUrl;
 
     private String iconImageUrl;
+    private LocalDate namingUpdatedAt;
 
     public static Team of(TeamCreateRequest request, University univ) {
         return Team.builder()
                 .name(request.name())
                 .category(request.category())
                 .univ(univ)
+                .namingUpdatedAt(null)
                 .iconImageUrl(request.iconImageUrl())
                 .build();
     }
 
     public void updateName(final String name) {
+        if (!canChangeName()) {
+            throw new TeamException(TOO_SHORT_PERIOD);
+        }
         this.name = name;
+        this.namingUpdatedAt = LocalDate.now();
     }
 
     public void setIconImageUrl(final String url) {
@@ -64,5 +74,13 @@ public class Team extends BaseTime {
 
     public boolean isDefaultImage() {
         return this.getIconImageUrl().isBlank();
+    }
+
+    private boolean canChangeName() {
+        if (namingUpdatedAt == null) {
+            return true;
+        }
+        long daysBetween = ChronoUnit.DAYS.between(namingUpdatedAt, LocalDate.now());
+        return daysBetween >= 30;
     }
 }
