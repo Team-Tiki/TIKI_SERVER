46808d98d8cf6e575d9a2461083727947fe91f70
smg0725 <smg0725@naver.com>
2024-12-03 02:44:57 +0900

    [CHORE] 메세지 추가

--- src/main/java/com/tiki/server/memberteammanager/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/memberteammanager/message/SuccessMessage.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum SuccessMessage {
 
 	UPDATE_NAME("팀 내 이름 변경 성공"),
+	LEAVE_TEAM("팀 탈퇴 성공"),
+	KICK_TEAM("팀 퇴출 성공"),
 	GET_POSITION("직책 불러오기 성공");
 
 	private final String message;
