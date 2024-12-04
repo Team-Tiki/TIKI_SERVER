f7810c16915d5b0687b66f869ba3f640c6a4aa88
Chan531 <chan657312@gmail.com>
2024-12-04 18:22:48 +0900

    feat(SubscribeInfo): 구독 정보 클래스

---
+++ src/main/java/com/tiki/server/team/entity/SubscribeInfo.java
@@ -0,0 +1,33 @@
+package com.tiki.server.team.entity;
+
+import static com.tiki.server.common.Constants.INIT_NUM;
+import static com.tiki.server.team.entity.Subscribe.*;
+import static jakarta.persistence.EnumType.STRING;
+import static lombok.AccessLevel.*;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@Builder(access = PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class SubscribeInfo {
+
+	@Enumerated(value = STRING)
+	private Subscribe subscribe;
+
+	private double usage;
+
+	public static SubscribeInfo createBasicSubscribe() {
+		return SubscribeInfo.builder()
+			.subscribe(BASIC)
+			.usage(INIT_NUM)
+			.build();
+	}
+}
