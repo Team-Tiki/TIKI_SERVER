fc53d01a301da53d614a5ae0c44737a4b265342b
Chan531 <chan657312@gmail.com>
2024-12-04 18:32:28 +0900

    feat(UsageGetResponse): 팀 용량 정보 조회 dto

---
+++ src/main/java/com/tiki/server/team/controller/dto/response/UsageGetResponse.java
@@ -0,0 +1,19 @@
+package com.tiki.server.team.controller.dto.response;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.Builder;
+
+@Builder(access = PRIVATE)
+public record UsageGetResponse(
+	double capacity,
+	double usage
+) {
+
+	public static UsageGetResponse of(final double capacity, final double usage) {
+		return UsageGetResponse.builder()
+			.capacity(capacity)
+			.usage(usage)
+			.build();
+	}
+}
