06fa8a20d67e8e15c1fab2b0e8c4940e43d705f2
Chan531 <chan657312@gmail.com>
2024-12-10 17:07:37 +0900

    feat(DocumentCreateRequest): 스웨거 정보 추가

--- src/main/java/com/tiki/server/document/dto/request/DocumentCreateRequest.java
+++ src/main/java/com/tiki/server/document/dto/request/DocumentCreateRequest.java
@@ -1,10 +1,14 @@
 package com.tiki.server.document.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.NonNull;
 
 public record DocumentCreateRequest(
+	@Schema(description = "파일 이름", example = "tiki.jpg")
 	@NonNull String fileName,
+	@Schema(description = "파일 url", example = "https://.../tiki.jpg")
 	@NonNull String fileUrl,
+	@Schema(description = "파일 용량", example = "1.23")
 	double capacity
 ) {
 }
