fbeb7a11992ef9ca4cddf3264f0712f73cbed33e
Chan531 <chan657312@gmail.com>
2024-12-10 17:15:40 +0900

    feat(FolderCreateRequest): 스웨거 정보 추가

--- src/main/java/com/tiki/server/folder/dto/request/FolderCreateRequest.java
+++ src/main/java/com/tiki/server/folder/dto/request/FolderCreateRequest.java
@@ -1,8 +1,10 @@
 package com.tiki.server.folder.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.NonNull;
 
 public record FolderCreateRequest(
+	@Schema(description = "폴더 이름", example = "폴더 1")
 	@NonNull String name
 ) {
 }
