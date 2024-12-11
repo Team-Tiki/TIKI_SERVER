9f4435b990952e282a8b943a8f5b8a0e5882a705
Chan531 <chan657312@gmail.com>
2024-12-11 19:02:52 +0900

    feat(TimeBlockRepository): id와 팀 id로 조회

--- src/main/java/com/tiki/server/timeblock/repository/TimeBlockRepository.java
+++ src/main/java/com/tiki/server/timeblock/repository/TimeBlockRepository.java
@@ -1,6 +1,7 @@
 package com.tiki.server.timeblock.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ import com.tiki.server.team.entity.Team;
 import com.tiki.server.timeblock.entity.TimeBlock;
 
 public interface TimeBlockRepository extends JpaRepository<TimeBlock, Long> {
+	Optional<TimeBlock> findByIdAndTeamId(long id, long teamId);
 
 	void deleteAllByTeamId(long teamId);
 
