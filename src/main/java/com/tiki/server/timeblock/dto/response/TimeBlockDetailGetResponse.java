5219880384ff966e28f9a37f7c9ec30385666d35
Chan531 <chan657312@gmail.com>
2024-12-11 18:36:53 +0900

    refactor(TimeBlockDetailGetResponse): 파라미터 및 데이터 매핑 로직 수정

--- src/main/java/com/tiki/server/timeblock/dto/response/TimeBlockDetailGetResponse.java
+++ src/main/java/com/tiki/server/timeblock/dto/response/TimeBlockDetailGetResponse.java
@@ -4,52 +4,53 @@ import static lombok.AccessLevel.PRIVATE;
 
 import java.util.List;
 
-import com.tiki.server.document.vo.DocumentVO;
+import com.tiki.server.document.entity.Document;
 
 import com.tiki.server.note.entity.Note;
+
 import lombok.Builder;
 import lombok.NonNull;
 
 @Builder(access = PRIVATE)
 public record TimeBlockDetailGetResponse(
-        List<DocumentGetResponse> documents,
-        List<NoteNameGetResponse> notes
+	List<DocumentGetResponse> documents,
+	List<NoteNameGetResponse> notes
 ) {
 
-    public static TimeBlockDetailGetResponse from(List<DocumentVO> documents, List<Note> notes) {
-        return TimeBlockDetailGetResponse.builder()
-                .documents(documents.stream().map(DocumentGetResponse::from).toList())
-                .notes(notes.stream().map(NoteNameGetResponse::from).toList())
-                .build();
-    }
+	public static TimeBlockDetailGetResponse from(List<Document> documents, List<Note> notes) {
+		return TimeBlockDetailGetResponse.builder()
+			.documents(documents.stream().map(DocumentGetResponse::from).toList())
+			.notes(notes.stream().map(NoteNameGetResponse::from).toList())
+			.build();
+	}
 
-    @Builder(access = PRIVATE)
-    public record DocumentGetResponse(
-            long documentId,
-            @NonNull String fileName,
-            @NonNull String fileUrl
-    ) {
+	@Builder(access = PRIVATE)
+	public record DocumentGetResponse(
+		long documentId,
+		@NonNull String fileName,
+		@NonNull String fileUrl
+	) {
 
-        public static DocumentGetResponse from(DocumentVO document) {
-            return DocumentGetResponse.builder()
-                    .documentId(document.documentId())
-                    .fileName(document.fileName())
-                    .fileUrl(document.fileUrl())
-                    .build();
-        }
-    }
+		public static DocumentGetResponse from(Document document) {
+			return DocumentGetResponse.builder()
+				.documentId(document.getId())
+				.fileName(document.getFileName())
+				.fileUrl(document.getFileUrl())
+				.build();
+		}
+	}
 
-    @Builder(access = PRIVATE)
-    public record NoteNameGetResponse(
-            long noteId,
-            @NonNull String noteName
-    ) {
+	@Builder(access = PRIVATE)
+	public record NoteNameGetResponse(
+		long noteId,
+		@NonNull String noteName
+	) {
 
-        public static NoteNameGetResponse from(final Note note) {
-            return NoteNameGetResponse.builder()
-                    .noteId(note.getId())
-                    .noteName(note.getTitle())
-                    .build();
-        }
-    }
+		public static NoteNameGetResponse from(final Note note) {
+			return NoteNameGetResponse.builder()
+				.noteId(note.getId())
+				.noteName(note.getTitle())
+				.build();
+		}
+	}
 }
