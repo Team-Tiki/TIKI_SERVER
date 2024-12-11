2db1ea332b3d95cfa6a850d9893392b52f9fb270
Chan531 <chan657312@gmail.com>
2024-12-11 19:25:38 +0900

    feat(ErrorCode): 유효하지 않은 파일 태그 메세지 추가

--- src/main/java/com/tiki/server/timeblock/message/ErrorCode.java
+++ src/main/java/com/tiki/server/timeblock/message/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
 
 	/* 400 BAD_REQUEST : 잘못된 요청 */
 	INVALID_TYPE(BAD_REQUEST, "유효한 타입이 아닙니다."),
+	INVALID_DOCUMENT_TAG(BAD_REQUEST, "유효한 파일이 아닙니다"),
 
 	/* 404 NOT_FOUND : 자원을 찾을 수 없음 */
 	INVALID_TIME_BLOCK(NOT_FOUND, "유효하지 않은 타임 블록입니다.");
