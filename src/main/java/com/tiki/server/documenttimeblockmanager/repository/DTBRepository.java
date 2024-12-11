2aa1184e68db3271fda20d9e3844767e73c9bb02
Chan531 <chan657312@gmail.com>
2024-12-11 18:14:04 +0900

    feat(DTBRepository): 파일과 연관된 데이터 제거

--- src/main/java/com/tiki/server/documenttimeblockmanager/repository/DTBRepository.java
+++ src/main/java/com/tiki/server/documenttimeblockmanager/repository/DTBRepository.java
@@ -1,9 +1,13 @@
 package com.tiki.server.documenttimeblockmanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tiki.server.documenttimeblockmanager.entity.DTBManager;
 
 public interface DTBRepository extends JpaRepository<DTBManager, Long> {
 	void deleteAllByTimeBlockId(long timeBlockId);
+
+	void deleteAllByDocumentIdIn(List<Long> documentIds);
 }
