e0ac961ea4bed6ede607daa95ff571f6321834b6
smg0725 <smg0725@naver.com>
2024-12-16 19:26:17 +0900

    [FEAT] 성공 메세지 추가

--- src/main/java/com/tiki/server/team/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/team/message/SuccessMessage.java
@@ -13,6 +13,7 @@ public enum SuccessMessage {
 	SUCCESS_ALTER_AUTHORITY("어드민 권한 위임 성공"),
 	SUCCESS_GET_TEAMS("전체 팀 불러오기 성공"),
 	SUCCESS_GET_CATEGORIES("카테고리 리스트 불러오기 성공"),
+	SUCCESS_GET_TEAM_INFORM("팀 설정 정보 불러오기 성공"),
 	SUCCESS_GET_JOINED_TEAM("소속 팀 불러오기 성공"),
 	SUCCESS_GET_CAPACITY_INFO("팀 용량 정보 조회 성공");
 
