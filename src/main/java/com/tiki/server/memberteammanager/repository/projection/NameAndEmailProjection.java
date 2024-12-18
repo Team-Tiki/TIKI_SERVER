16c0e5ee62eef6c4ddde5f415d2125f67912767c
smg0725 <smg0725@naver.com>
2024-12-18 16:57:49 +0900

    [FEAT] 팀원 불러오기 프로젝션 구현

--- src/main/java/com/tiki/server/memberteammanager/repository/projection/NameAndEmailProjection.java
+++
@@ -1,8 +0,0 @@
-package com.tiki.server.memberteammanager.repository.projection;
-
-import com.tiki.server.common.entity.Email;
-
-public interface NameAndEmailProjection {
-    String getName();
-    String getEmail();
-}
