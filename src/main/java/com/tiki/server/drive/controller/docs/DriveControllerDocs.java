7437d1a5c3acc576cb46468d41c5b136fc950fb3
Chan531 <chan657312@gmail.com>
2024-12-18 15:28:13 +0900

    feat: 드라이브 조회 api 스웨거

---
+++ src/main/java/com/tiki/server/drive/controller/docs/DriveControllerDocs.java
@@ -0,0 +1,51 @@
+package com.tiki.server.drive.controller.docs;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tiki.server.common.dto.ErrorResponse;
+import com.tiki.server.common.dto.SuccessResponse;
+import com.tiki.server.drive.dto.DriveGetResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+public interface DriveControllerDocs {
+
+	@Operation(
+		summary = "드라이브 조회",
+		description = "드라이브 뷰를 조회한다.",
+		responses = {
+			@ApiResponse(responseCode = "200", description = "성공"),
+			@ApiResponse(
+				responseCode = "4xx",
+				description = "클라이언트(요청) 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "500",
+				description = "서버 내부 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+	)
+	SuccessResponse<DriveGetResponse> getDrive(
+		@Parameter(hidden = true) Principal principal,
+		@Parameter(
+			name = "teamId",
+			description = "팀 id",
+			in = ParameterIn.PATH,
+			required = true,
+			example = "1"
+		) @PathVariable long teamId,
+		@Parameter(
+			name = "folderId",
+			description = "조회할 폴더 id (최상단은 비워두기)",
+			in = ParameterIn.QUERY,
+			example = "1"
+		) @RequestParam Long folderId
+	);
+}
