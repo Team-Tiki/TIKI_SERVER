8653046800ea9d0ec8c9d7fe941e430ef299b4e0
paragon0107 <smg0725@naver.com>
2024-12-31 15:30:28 +0900

    [FEAT] 팀원 초대 서비스 부분 구현

--- src/main/java/com/tiki/server/email/emailsender/service/EmailSenderService.java
+++ src/main/java/com/tiki/server/email/emailsender/service/EmailSenderService.java
@@ -1,11 +1,20 @@
 package com.tiki.server.email.emailsender.service;
 
+import com.tiki.server.common.entity.Position;
 import com.tiki.server.email.emailsender.entity.MailSender;
 import com.tiki.server.email.emailsender.service.dto.EmailServiceRequest;
+import com.tiki.server.email.emailsender.service.dto.TeamInvitationCreateServiceRequest;
+import com.tiki.server.email.teaminvitation.adapter.TeamInvitationFinder;
+import com.tiki.server.email.teaminvitation.adapter.TeamInvitationSaver;
+import com.tiki.server.email.teaminvitation.entity.TeamInvitation;
 import com.tiki.server.email.verification.adapter.EmailVerificationSaver;
 import com.tiki.server.email.verification.domain.EmailVerification;
 import com.tiki.server.member.adapter.MemberFinder;
 import com.tiki.server.member.entity.Member;
+import com.tiki.server.memberteammanager.adapter.MemberTeamManagerFinder;
+import com.tiki.server.memberteammanager.entity.MemberTeamManager;
+import com.tiki.server.team.adapter.TeamFinder;
+import com.tiki.server.team.entity.Team;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,10 +27,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class EmailSenderService {
 
     private final MemberFinder memberFinder;
-    private final EmailVerificationSaver emailVerificationSaver;
+    private final TeamFinder teamFinder;
     private final MailSender mailSender;
+    private final EmailVerificationSaver emailVerificationSaver;
+    private final MemberTeamManagerFinder memberTeamManagerFinder;
+    private final TeamInvitationSaver teamInvitationSaver;
+    private final TeamInvitationFinder teamInvitationFinder;
 
-    @Transactional
     public void sendSignUp(final EmailServiceRequest emailServiceRequest) {
         memberFinder.checkPresent(emailServiceRequest.email());
         EmailVerification emailVerification = EmailVerification.of(emailServiceRequest.email());
@@ -32,7 +44,6 @@ public class EmailSenderService {
         emailVerificationSaver.save(emailVerification);
     }
 
-    @Transactional
     public void sendPasswordChanging(final EmailServiceRequest emailServiceRequest) {
         Member member = memberFinder.checkEmpty(emailServiceRequest.email());
         EmailVerification emailVerification = EmailVerification.of(emailServiceRequest.email());
@@ -42,5 +53,19 @@ public class EmailSenderService {
                 member.getName()
         );
         emailVerificationSaver.save(emailVerification);
+    }
+
+    public void createTeamInvitation(final TeamInvitationCreateServiceRequest request) {
+        MemberTeamManager memberTeamManager = memberTeamManagerFinder.findByMemberIdAndTeamId(request.senderId(),
+                request.teamId());
+        memberTeamManager.checkMemberAccessible(Position.ADMIN);
+        Team team = teamFinder.findById(request.teamId());
+        mailSender.sendTeamInvitationMail(
+                request.email().getEmail(),
+                memberTeamManager.getName(),
+                team.getName(),
+                request.teamId()
+        );
+        teamInvitationSaver.createTeamInvitation(TeamInvitation.of(request.teamId(), request.email()));
     }
 }
