1d3aea26e33d83a3c8780ee9599c6b928efc8e3e
paragon0107 <smg0725@naver.com>
2024-12-31 15:31:23 +0900

    [CHORE] 중복 DTO 삭제

--- src/main/java/com/tiki/server/email/teaminvitation/controller/dto/TeamInvitationCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.tiki.server.email.teaminvitation.controller.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record TeamInvitationCreateRequest(
-        @NotNull String email
-) {
-}
