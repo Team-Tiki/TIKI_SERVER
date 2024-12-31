c31a2b6ae6764eb323fb6169d4324c0a86c9e7ac
paragon0107 <smg0725@naver.com>
2024-12-31 15:30:15 +0900

    [FEAT] 팀원 초대 컨트롤러 부분 구현

--- src/main/java/com/tiki/server/email/emailsender/controller/EmailSenderController.java
+++ src/main/java/com/tiki/server/email/emailsender/controller/EmailSenderController.java
@@ -7,12 +7,12 @@ import com.tiki.server.email.emailsender.controller.dto.request.EmailRequest;
 import com.tiki.server.email.emailsender.message.SuccessMessage;
 import com.tiki.server.email.emailsender.service.EmailSenderService;
 import com.tiki.server.email.emailsender.service.dto.EmailServiceRequest;
+import com.tiki.server.email.emailsender.service.dto.TeamInvitationCreateServiceRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 import static com.tiki.server.common.dto.SuccessResponse.success;
 import static com.tiki.server.common.support.UriGenerator.getUri;
@@ -34,6 +34,17 @@ public class EmailSenderController implements EmailSenderControllerDocs {
     @PostMapping("/verification/password")
     public ResponseEntity<BaseResponse> sendChangingPasswordMail(@RequestBody EmailRequest mailRequest) {
         emailSenderService.sendPasswordChanging(EmailServiceRequest.from(mailRequest.email()));
+        return ResponseEntity.created(getUri("/")).body(success(SuccessMessage.SUCCESS_SEND_EMAIL.getMessage()));
+    }
+
+    @PostMapping("/invitation/team/{teamId}")
+    public ResponseEntity<BaseResponse> sendInvitationMail(
+            Principal principal,
+            @PathVariable long teamId,
+            @RequestBody EmailRequest emailRequest
+    ) {
+        long memberId = Long.parseLong(principal.getName());
+        emailSenderService.createTeamInvitation(TeamInvitationCreateServiceRequest.of(emailRequest.email(),teamId,memberId));
         return ResponseEntity.created(getUri("/")).body(success(SuccessMessage.SUCCESS_SEND_EMAIL.getMessage()));
     }
 }
