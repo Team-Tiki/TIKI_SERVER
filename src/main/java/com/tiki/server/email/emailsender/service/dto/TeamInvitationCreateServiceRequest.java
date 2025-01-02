2c622951076711c445b14541edfac56dc842a3e6
smg0725 <smg0725@naver.com>
2025-01-03 02:15:36 +0900

    [CHORE] 파라미터 이름 수정

--- src/main/java/com/tiki/server/email/emailsender/service/dto/TeamInvitationCreateServiceRequest.java
+++ src/main/java/com/tiki/server/email/emailsender/service/dto/TeamInvitationCreateServiceRequest.java
@@ -4,12 +4,12 @@ import com.tiki.server.email.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record TeamInvitationCreateServiceRequest(
-        @NotNull Email email,
+        @NotNull Email targetEmail,
         @NotNull long teamId,
         @NotNull long senderId
         ) {
 
-    public static TeamInvitationCreateServiceRequest of(final String email,final long teamId,final long senderId ){
-        return new TeamInvitationCreateServiceRequest(Email.from(email), teamId, senderId);
+    public static TeamInvitationCreateServiceRequest of(final String targetEmail,final long teamId,final long senderId ){
+        return new TeamInvitationCreateServiceRequest(Email.from(targetEmail), teamId, senderId);
     }
 }
