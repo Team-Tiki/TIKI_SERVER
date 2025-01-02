0b58688db45a575cd1fbc4f24e1345fbb6ee2c89
paragon0107 <smg0725@naver.com>
2025-01-02 18:01:50 +0900

    [FEAT] TeamInvitation 어댑터 부분 구현

---
+++ src/main/java/com/tiki/server/email/teaminvitation/adapter/TeamInvitationDeleter.java
@@ -0,0 +1,23 @@
+package com.tiki.server.email.teaminvitation.adapter;
+
+import com.tiki.server.common.support.RepositoryAdapter;
+import com.tiki.server.email.teaminvitation.entity.TeamInvitation;
+import com.tiki.server.email.teaminvitation.repository.TeamInvitationRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RepositoryAdapter
+@RequiredArgsConstructor
+public class TeamInvitationDeleter {
+
+    private final TeamInvitationRepository teamInvitationRepository;
+
+    public void deleteTeamInvitation(final TeamInvitation teamInvitation) {
+        teamInvitationRepository.delete(teamInvitation);
+    }
+
+    public void deleteAll(List<TeamInvitation> expiredInvitation) {
+        teamInvitationRepository.deleteAll(expiredInvitation);
+    }
+}
