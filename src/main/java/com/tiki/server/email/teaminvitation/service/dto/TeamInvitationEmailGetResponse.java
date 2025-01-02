4a137199ff4017a1df11f11a35881d6624a106f9
smg0725 <smg0725@naver.com>
2025-01-03 02:15:48 +0900

    [CHORE] 반환 형식 수정

--- src/main/java/com/tiki/server/email/teaminvitation/service/dto/TeamInvitationEmailGetResponse.java
+++ src/main/java/com/tiki/server/email/teaminvitation/service/dto/TeamInvitationEmailGetResponse.java
@@ -5,9 +5,9 @@ import com.tiki.server.email.teaminvitation.entity.TeamInvitation;
 import jakarta.validation.constraints.NotNull;
 
 public record TeamInvitationEmailGetResponse(
-        @NotNull Email email
+        @NotNull String email
 ) {
     public static TeamInvitationEmailGetResponse from(final TeamInvitation teamInvitation) {
-        return new TeamInvitationEmailGetResponse(teamInvitation.getEmail());
+        return new TeamInvitationEmailGetResponse(teamInvitation.getEmailToString());
     }
 }
