31859b40f6e2b293dd76abaa1b9019331abddb3a
Chan531 <chan657312@gmail.com>
2025-01-12 01:55:08 +0900

    feat: 타임블록 수정 성공 메세지

--- src/main/java/com/tiki/server/timeblock/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/timeblock/message/SuccessMessage.java
@@ -11,6 +11,7 @@ public enum SuccessMessage {
 	SUCCESS_GET_TIMELINE("타임라인 조회 성공"),
 	SUCCESS_GET_ALL_TIME_BLOCK("전체 타임블록 조회 성공"),
 	SUCCESS_GET_TIME_BLOCK_DETAIL("타임 블록 상세 정보 조회 성공"),
+	SUCCESS_UPDATE_TIME_BLOCK("타임 블록 정보 수정 성공"),
 	SUCCESS_CREATE_DOCUMENT_TAG("파일 태그 성공");
 
 	private final String message;
