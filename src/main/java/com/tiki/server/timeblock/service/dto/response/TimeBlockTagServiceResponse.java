3b45f6c732a828d3be911524b823c6ec3008ec48
paragon0107 <smg0725@naver.com>
2025-01-15 17:22:02 +0900

    [CHORE] dto데이터추가

--- src/main/java/com/tiki/server/timeblock/service/dto/response/TimeBlockTagServiceResponse.java
+++ src/main/java/com/tiki/server/timeblock/service/dto/response/TimeBlockTagServiceResponse.java
@@ -1,16 +1,27 @@
 package com.tiki.server.timeblock.service.dto.response;
 
+import com.tiki.server.timeblock.entity.BlockType;
 import com.tiki.server.timeblock.entity.TimeBlock;
 
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+
 public record TimeBlockTagServiceResponse(
         @NotNull long id,
         @NotNull String name,
-        @NotNull String color
+        @NotNull String color,
+        @NotNull BlockType blockType,
+        @NotNull LocalDate startDate
 ) {
 
-    public static TimeBlockTagServiceResponse from(final TimeBlock timeBlock){
-        return new TimeBlockTagServiceResponse(timeBlock.getId(), timeBlock.getName(), timeBlock.getColor());
+    public static TimeBlockTagServiceResponse from(final TimeBlock timeBlock) {
+        return new TimeBlockTagServiceResponse(
+                timeBlock.getId(),
+                timeBlock.getName(),
+                timeBlock.getColor(),
+                timeBlock.getType(),
+                timeBlock.getStartDate()
+        );
     }
 }
