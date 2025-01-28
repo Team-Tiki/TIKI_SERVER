542fc61fd360d80ddf898904b294bca018b752fa
Chan531 <chan657312@gmail.com>
2025-01-29 08:14:51 +0900

    feat: 회원 탈퇴 성공 메세지 추가

--- src/main/java/com/tiki/server/member/message/SuccessMessage.java
+++ src/main/java/com/tiki/server/member/message/SuccessMessage.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum SuccessMessage {
 
     SUCCESS_CREATE_MEMBER("회원가입 성공"),
-    SUCCESS_CHANGING_PASSWORD("비밀번호 변경 성공");
+    SUCCESS_CHANGING_PASSWORD("비밀번호 변경 성공"),
+    SUCCESS_WITHDRAWAL("회원 탈퇴 성공"),;
 
     private final String message;
 }
