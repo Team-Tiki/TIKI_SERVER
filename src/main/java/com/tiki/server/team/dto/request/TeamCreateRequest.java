11e775173f2b804da26e179f12513c9ab1947c55
smg0725 <smg0725@naver.com>
2025-02-03 20:47:53 +0900

    [CHORE] 쓸모없는 임포트문 제거

--- src/main/java/com/tiki/server/team/dto/request/TeamCreateRequest.java
+++ src/main/java/com/tiki/server/team/dto/request/TeamCreateRequest.java
@@ -4,17 +4,16 @@ import com.tiki.server.common.util.Validator;
 import com.tiki.server.team.entity.Category;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 
 public record TeamCreateRequest(
-	@NotNull String name,
-	@NotNull Category category,
-	@NotNull String iconImageUrl
+        @NotNull String name,
+        @NotNull Category category,
+        @NotNull String iconImageUrl
 ) {
-	public TeamCreateRequest(final String name, final Category category, final String iconImageUrl) {
-		Validator.validateLength(name, 30);
-		this.name = name;
-		this.category = category;
-		this.iconImageUrl = iconImageUrl;
-	}
+    public TeamCreateRequest(final String name, final Category category, final String iconImageUrl) {
+        Validator.validateLength(name, 30);
+        this.name = name;
+        this.category = category;
+        this.iconImageUrl = iconImageUrl;
+    }
 }
