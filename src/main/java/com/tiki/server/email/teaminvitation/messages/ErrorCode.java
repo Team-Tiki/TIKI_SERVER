d4b639dccebced57f112e15bcba79dddcf2d23af
smg0725 <smg0725@naver.com>
2025-03-01 21:31:16 +0900

    [CHORE] 예외 코드 추가

--- src/main/java/com/tiki/server/email/teaminvitation/messages/ErrorCode.java
+++ src/main/java/com/tiki/server/email/teaminvitation/messages/ErrorCode.java
@@ -11,6 +11,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    ALREADY_INVITED(BAD_REQUEST, "이미 초대한 이메일 입니다."),
     ALREADY_INVITED_MEMBER(BAD_REQUEST, "이미 존재하는 팀원입니다."),
     NOT_MATCHED_MEMBER_INFORM(BAD_REQUEST, "일치하지 않은 초대정보 입니다."),
     INVALID_TEAM_INVITATION(NOT_FOUND, "존재하지 않거나 만료된 초대정보입니다.");
