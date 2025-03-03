c30db20f491ba885943db5897397714d0e03661a
Chan531 <chan657312@gmail.com>
2025-03-03 17:54:20 +0900

    [CHORE] dto값 fileName에서 fileKey로 수정

--- src/main/java/com/tiki/server/external/dto/response/PreSignedUrlResponse.java
+++ src/main/java/com/tiki/server/external/dto/response/PreSignedUrlResponse.java
@@ -7,13 +7,13 @@ import lombok.Builder;
 
 @Builder(access = PRIVATE)
 public record PreSignedUrlResponse(
-	@NotNull String fileName,
+	@NotNull String fileKey,
 	@NotNull String url
 ) {
 
-	public static PreSignedUrlResponse of(final String fileName, final String url) {
+	public static PreSignedUrlResponse of(final String fileKey, final String url) {
 		return PreSignedUrlResponse.builder()
-			.fileName(fileName)
+			.fileKey(fileKey)
 			.url(url)
 			.build();
 	}
