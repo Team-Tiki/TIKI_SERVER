3fd017e94e928769004842a5ecee5d6821c175a8
Chan531 <chan657312@gmail.com>
2025-03-03 18:43:52 +0900

    [CHORE] dto값 url에서 key로 수정

--- src/main/java/com/tiki/server/team/dto/request/TeamCreateRequest.java
+++ src/main/java/com/tiki/server/team/dto/request/TeamCreateRequest.java
@@ -8,13 +8,13 @@ import jakarta.validation.constraints.NotNull;
 public record TeamCreateRequest(
         @NotNull String name,
         @NotNull Category category,
-        @NotNull String iconImageUrl
+        @NotNull String iconImageKey
 ) {
-    public TeamCreateRequest(final String name, final Category category, final String iconImageUrl) {
+    public TeamCreateRequest(final String name, final Category category, final String iconImageKey) {
         Validator.validText(name);
         Validator.validateLength(name, 30);
         this.name = name;
         this.category = category;
-        this.iconImageUrl = iconImageUrl;
+        this.iconImageKey = iconImageKey;
     }
 }
