02319022261caee2ea24993f9cc6e3f8c2ec7306
smg0725 <smg0725@naver.com>
2025-03-23 19:21:48 +0900

    Revert problematic commits

--- src/main/java/com/tiki/server/email/teaminvitation/entity/TeamInvitation.java
+++ src/main/java/com/tiki/server/email/teaminvitation/entity/TeamInvitation.java
@@ -1,27 +1,33 @@
 package com.tiki.server.email.teaminvitation.entity;
 
 import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
 
 import com.tiki.server.common.entity.BaseTime;
 import com.tiki.server.email.Email;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.redis.core.RedisHash;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Getter
+@Entity
 @Builder
+@NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PRIVATE)
-@RedisHash(value = "TeamInvitation", timeToLive = 604800)
 public class TeamInvitation extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @Column(name = "invitation_id")
+    private Long id;
 
     private String sender;
 
@@ -32,11 +38,10 @@ public class TeamInvitation extends BaseTime {
     private LocalDate expiredDate;
 
     public static TeamInvitation of(final String sender, final long teamId, final Email email) {
-        return TeamInvitation.builder().id(teamId + email.getEmail()).sender(sender).teamId(teamId).email(email)
-                .expiredDate(LocalDate.now()).build();
+        return TeamInvitation.builder().sender(sender).teamId(teamId).email(email).expiredDate(LocalDate.now()).build();
     }
 
-    public String getEmailToString() {
+    public String getEmailToString(){
         return email.getEmail();
     }
 }
